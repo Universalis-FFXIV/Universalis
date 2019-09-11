610276d645913f22d2baee0359533e2a76a93615
karashiiro <49822414+karashiiro@users.noreply.github.com>
2019-09-11 13:48:00 -0700

    Log everything to the console as well

--- src/universalis.ts
+++ src/universalis.ts
@@ -42,7 +42,8 @@ const logger = winston.createLogger({
         new winston.transports.File({
             filename: "logs/error.log",
             level: "error"
-        })
+        }),
+        new winston.transports.Console()
     ]
 });
 logger.info("Process started.");
