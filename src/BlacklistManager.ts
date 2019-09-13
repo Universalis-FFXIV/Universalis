259a4af84ae760423f407799b5ae8547bf7e6946
karashiiro <49822414+karashiiro@users.noreply.github.com>
2019-09-12 22:53:20 -0700

    refuse data from bad actors

--- src/BlacklistManager.ts
+++ src/BlacklistManager.ts
@@ -1,3 +1,20 @@
+import { Collection } from "mongodb";
+
 export class BlacklistManager {
-    
+    private blacklist: Collection;
+
+    constructor(blacklist: Collection) {
+        this.blacklist = blacklist;
+    }
+
+    /** Add an uploader to the blacklist, preventing their data from being processed. */
+    public async add(uploaderID: string): Promise<void> {
+        await this.blacklist.insertOne({ uploaderID });
+    }
+
+    /** Check if the blacklist has an uploader. */
+    public async has(uploaderID: string): Promise<boolean> {
+        const exists = await this.blacklist.findOne({ uploaderID });
+        return exists ? true : false;
+    }
 }
