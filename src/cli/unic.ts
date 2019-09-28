6372622408b36dc10b0e20add6d27176cf580c0c
karashiiro <49822414+karashiiro@users.noreply.github.com>
2019-09-28 00:09:02 -0700

    some cli work

--- src/cli/unic.ts
+++ src/cli/unic.ts
@@ -1,14 +1,25 @@
 import chalk from "chalk";
+import { MongoClient } from "mongodb";
 import readline from "readline";
+import sha from "sha.js";
+
+const db = MongoClient.connect("mongodb://localhost:27017/", { useNewUrlParser: true, useUnifiedTopology: true });
 
 const stdin = readline.createInterface({
     completer: null,
     input: process.stdin,
-    output: process.stdout,
     prompt: chalk.cyan("> "),
     terminal: true,
 });
 
 stdin.on("line", (line) => {
+    const args = line.split(/\s+/g);
+    const command = args.pop();
 
+    if (command === "addkey") {
+        if (args.length !== 2) {
+            return console.log(chalk.bgRedBright("Missing arguments."));
+        }
+        return console.log(chalk.bgGreenBright("Success."));
+    }
 });
