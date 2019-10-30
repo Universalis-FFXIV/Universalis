f7607cdea6bf61edecb1254dddd0446c24cd7322
karashiiro <49822414+karashiiro@users.noreply.github.com>
2019-10-30 16:41:09 -0700

    Add average price field to item response in API

--- src/endpoints/parseListings.ts
+++ src/endpoints/parseListings.ts
@@ -35,6 +35,13 @@ export async function parseListings(ctx: ParameterizedContext, worldMap: Map<str
             });
         }
         if (Array.isArray(item.listings)) {
+            item.averagePrice = item.listings.flat((listing: MarketBoardItemListing) => listing.pricePerUnit);
+            item.averagePriceNQ = item.listings
+                .filter((listing: MarketBoardItemListing) => !listing.hq)
+                .flat((listing: MarketBoardItemListing) => listing.pricePerUnit);
+            item.averagePriceHQ = item.listings
+                .filter((listing: MarketBoardItemListing) => listing.hq)
+                .flat((listing: MarketBoardItemListing) => listing.pricePerUnit);
             for (const listing of item.listings) {
                 listing.isCrafted =
                     listing.creatorID !== "5feceb66ffc86f38d952786c6d696c79c2dbc239dd4e91b46729d73a27fb57e9";
