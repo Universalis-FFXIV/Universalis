76eb2c6634e862b973b159e7174f447d51460d8e
karashiiro <49822414+karashiiro@users.noreply.github.com>
2019-11-07 08:17:21 -0800

    Remove restriction, it was TC anyways

--- src/endpoints/upload.ts
+++ src/endpoints/upload.ts
@@ -116,7 +116,7 @@ export async function upload(parameters: UploadProcessParameters) {
         ));
     }
 
-    if (uploadData.marketTaxRates && trustedSource.sourceName === "FFXIV Teamcraft") {
+    if (uploadData.marketTaxRates) {
         promises.push(extraDataManager.setTaxRates(uploadData.marketTaxRates));
     }
 
