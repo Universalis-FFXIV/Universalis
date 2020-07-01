160dcf6889d43a3e3c2d8ba8bd39839c8cb46432
karashiiro <49822414+karashiiro@users.noreply.github.com>
2020-06-30 21:59:41 -0700

    update regex for CN charset

--- src/validate.ts
+++ src/validate.ts
@@ -486,7 +486,7 @@ export async function writeOutObject(logger: Logger, obj: any) {
 
 export function removeUnsafeCharacters(input: string): string {
 	return input.replace(
-		/[^a-zA-Z0-9'\- ⺀-⺙⺛-⻳⼀-⿕々〇〡-〩〸-〺〻㐀-䶵一-鿃豈-鶴侮-頻並-龎]/gu,
+		/[^a-zA-Z0-9'\- ·⺀-⺙⺛-⻳⼀-⿕々〇〡-〩〸-〺〻㐀-䶵一-鿃豈-鶴侮-頻並-龎]/gu,
 		"",
 	);
 }
