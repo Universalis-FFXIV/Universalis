7cc3c3659475834770349810c52d8ac777a26d6a
karashiiro <49822414+karashiiro@users.noreply.github.com>
2020-08-28 17:13:09 -0700

    draft cleanup script

---
+++ src/scripts/remove-duplicates.ts
@@ -0,0 +1,43 @@
+import { MongoClient } from "mongodb";
+import { createLogger } from "winston";
+import { initializeWorldMappings } from "../initializeWorldMappings";
+import { RemoteDataManager } from "../remote/RemoteDataManager";
+
+const db = MongoClient.connect("mongodb://localhost:27017/", {
+	useNewUrlParser: true,
+	useUnifiedTopology: true,
+});
+
+const worldMap: Map<string, number> = new Map();
+const worldIDMap: Map<number, string> = new Map();
+
+(async () => {
+	const logger = createLogger();
+
+	const remoteDataManager = new RemoteDataManager({ logger });
+	await remoteDataManager.fetchAll();
+
+	const marketableItemIDs = await remoteDataManager.getMarketableItemIDs();
+
+	await initializeWorldMappings(worldMap, worldIDMap);
+
+	const universalisDB = (await db).db("universalis");
+	const recentData = universalisDB.collection("recentData");
+
+	for (const [worldID] of worldIDMap) {
+		for (const itemID of marketableItemIDs) {
+			const data = recentData.find({ worldID, itemID });
+			if ((await data.count()) > 1) {
+				data
+					.sort((a: any, b: any) => b.lastUploadTime - a.lastUploadTime)
+					.skip(1);
+				let record: any;
+				// tslint:disable-next-line: no-conditional-assignment
+				while ((record = data.read(1))) {
+					// tslint:disable-next-line: no-console
+					console.log(record);
+				}
+			}
+		}
+	}
+})();
