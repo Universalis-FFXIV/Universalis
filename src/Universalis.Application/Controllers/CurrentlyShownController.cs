0bffb75c1e62b42eb5b09b485aa705c7c3571434
karashiiro <49822414+karashiiro@users.noreply.github.com>
2021-06-25 08:06:13 -0700

    Add listing endpoint controller

---
+++ src/Universalis.Application/Controllers/CurrentlyShownController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Universalis.GameData;
+
+namespace Universalis.Application.Controllers
+{
+    [Route("api/{itemId}/{worldOrDc}/[controller]")]
+    [ApiController]
+    public class CurrentlyShownController : ControllerBase
+    {
+        private readonly IGameDataProvider _gameData;
+
+        public CurrentlyShownController(IGameDataProvider gameData)
+        {
+            _gameData = gameData;
+        }
+
+        [HttpGet]
+        public ActionResult<string> Get(uint itemId, string worldOrDc)
+        {
+            if (!_gameData.MarketableItemIds().Contains(itemId))
+                return NotFound();
+
+            return worldOrDc;
+        }
+    }
+}
