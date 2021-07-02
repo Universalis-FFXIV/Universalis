3e6c7f5d2198e0753f6564dcb6e5c7d1dcf8b1aa
karashiiro <49822414+karashiiro@users.noreply.github.com>
2021-07-02 07:16:30 -0700

    Add HistoryController

---
+++ src/Universalis.Application/Controllers/HistoryController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Universalis.Application.Common;
+using Universalis.GameData;
+
+namespace Universalis.Application.Controllers
+{
+    [Route("api/history/{itemId}/{worldOrDc}/[controller]")]
+    [ApiController]
+    public class HistoryController : ControllerBase
+    {
+        private readonly IGameDataProvider _gameData;
+
+        public HistoryController(IGameDataProvider gameData)
+        {
+            _gameData = gameData;
+        }
+
+        [HttpGet]
+        public ActionResult<string> Get(uint itemId, string worldOrDc)
+        {
+            if (!_gameData.MarketableItemIds().Contains(itemId) || worldOrDc.Length == 0)
+                return NotFound();
+
+            var worldDc = WorldDc.From(worldOrDc, _gameData);
+
+            return worldDc.IsWorld ? worldDc.WorldId.ToString() : worldDc.DcName;
+        }
+    }
+}
