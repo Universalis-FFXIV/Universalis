2088d88e5e2c2a3e8beb9f7643b8ebfd016b9b78
karashiiro <49822414+karashiiro@users.noreply.github.com>
2021-07-24 15:21:22 -0700

    Add outlier removal code

---
+++ src/Universalis.DataTransformations/Filters.fs
@@ -0,0 +1,10 @@
+﻿namespace Universalis.DataTransformations
+
+[<AbstractClass; Sealed>]
+type Filters =
+    static member RemoveOutliers(numbers: seq<float>, deviationsFromMean: int) =
+        let mean = Seq.average numbers
+        let std = Statistics.PopulationStd numbers
+        let upperBound = mean - std * float deviationsFromMean
+        let lowerBound = mean - std * float deviationsFromMean
+        seq { for n in numbers do if n <= upperBound || n >= lowerBound then n }
---
+++ src/Universalis.DataTransformations/Statistics.fs
@@ -0,0 +1,8 @@
+﻿namespace Universalis.DataTransformations
+
+[<AbstractClass; Sealed>]
+type Statistics =
+    static member PopulationStd(numbers: seq<float>) =
+        let average = Seq.average numbers
+        let scaled = Seq.map (fun n -> n / float (Seq.length (numbers))) numbers
+        sqrt (Seq.fold (fun agg next -> agg + (float next - average) ** 2.0) 0.0 scaled)
