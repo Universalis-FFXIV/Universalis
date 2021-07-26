ebf8990df463459d92463cd391450fe2b21154a9
karashiiro <49822414+karashiiro@users.noreply.github.com>
2021-07-26 12:06:14 -0700

    Implement database code for trusted sources

---
+++ src/Universalis.DbAccess/Query/TrustedSourceQuery.cs
@@ -0,0 +1,17 @@
+﻿using MongoDB.Driver;
+using Universalis.Entities.Uploaders;
+
+namespace Universalis.DbAccess.Query
+{
+    public class TrustedSourceQuery : DbAccessQuery<TrustedSource>
+    {
+        public string ApiKeyHash { get; set; }
+
+        internal override FilterDefinition<TrustedSource> ToFilterDefinition()
+        {
+            var filterBuilder = Builders<TrustedSource>.Filter;
+            var filter = filterBuilder.Eq(o => o.ApiKeySha256, ApiKeyHash);
+            return filter;
+        }
+    }
+}
---
+++ src/Universalis.DbAccess/TrustedSourceDbAccess.cs
@@ -0,0 +1,10 @@
+﻿using Universalis.DbAccess.Query;
+using Universalis.Entities.Uploaders;
+
+namespace Universalis.DbAccess
+{
+    public class TrustedSourceDbAccess : DbAccessService<TrustedSource, TrustedSourceQuery>
+    {
+        public TrustedSourceDbAccess() : base("universalis", "trustedSources") { }
+    }
+}
