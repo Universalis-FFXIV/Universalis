28e260cffd132e5c11da69666a6a9f89f0c0959c
karashiiro <49822414+karashiiro@users.noreply.github.com>
2021-07-26 10:56:01 -0700

    Implement database code for history

---
+++ src/Universalis.DbAccess/HistoryDbAccess.cs
@@ -0,0 +1,10 @@
+﻿using Universalis.DbAccess.Query;
+using Universalis.Entities.MarketBoard;
+
+namespace Universalis.DbAccess
+{
+    public class HistoryDbAccess : DbAccessService<History, HistoryQuery>
+    {
+        public HistoryDbAccess() : base("universalis", "extendedHistory") { }
+    }
+}
