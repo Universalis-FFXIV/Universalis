ef0b02ff561c4fca207aeb5582015fb9ff1e1a7e
karashiiro <49822414+karashiiro@users.noreply.github.com>
2021-07-27 11:14:00 -0700

    Style tweak

--- build/Configuration.cs
+++ build/Configuration.cs
@@ -1,13 +1,11 @@
-using System;
-using System.ComponentModel;
-using System.Linq;
 using Nuke.Common.Tooling;
+using System.ComponentModel;
 
 [TypeConverter(typeof(TypeConverter<Configuration>))]
 public class Configuration : Enumeration
 {
-    public static Configuration Debug = new Configuration { Value = nameof(Debug) };
-    public static Configuration Release = new Configuration { Value = nameof(Release) };
+    public static Configuration Debug = new() { Value = nameof(Debug) };
+    public static Configuration Release = new() { Value = nameof(Release) };
 
     public static implicit operator string(Configuration configuration)
     {
