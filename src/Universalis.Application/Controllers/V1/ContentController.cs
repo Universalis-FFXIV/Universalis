d7be4492faa1cd2e84cae5df9e66ea072cd39dc7
karashiiro <49822414+karashiiro@users.noreply.github.com>
2021-07-28 22:22:19 -0700

    Add content endpoint

---
+++ src/Universalis.Application/Controllers/V1/ContentController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+using Universalis.Application.Views;
+using Universalis.DbAccess;
+using Universalis.DbAccess.Queries;
+
+namespace Universalis.Application.Controllers.V1
+{
+    [Route("api/extra/content/{contentId}")]
+    [ApiController]
+    public class ContentController : ControllerBase
+    {
+        private readonly IContentDbAccess _contentDb;
+
+        public ContentController(IContentDbAccess contentDb)
+        {
+            _contentDb = contentDb;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Get(string contentId)
+        {
+            var content = await _contentDb.Retrieve(new ContentQuery { ContentId = contentId });
+            if (content == null)
+            {
+                return Ok(new ContentView());
+            }
+
+            return Ok(new ContentView
+            {
+                ContentId = content.ContentId,
+                ContentType = content.ContentType,
+                CharacterName = content.CharacterName,
+            });
+        }
+    }
+}
---
+++ src/Universalis.Application/Views/ContentView.cs
@@ -0,0 +1,16 @@
+﻿using Newtonsoft.Json;
+
+namespace Universalis.Application.Views
+{
+    public class ContentView
+    {
+        [JsonProperty("contentID")]
+        public string ContentId { get; set; }
+
+        [JsonProperty("contentType")]
+        public string ContentType { get; set; }
+
+        [JsonProperty("characterName", NullValueHandling = NullValueHandling.Ignore)]
+        public string CharacterName { get; set; }
+    }
+}
