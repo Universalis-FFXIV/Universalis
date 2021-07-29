e334e40cf882c32aa10a85aa9f3b73204a010b97
karashiiro <49822414+karashiiro@users.noreply.github.com>
2021-07-28 23:07:05 -0700

    Implement recently updated items endpoint

---
+++ src/Universalis.Application/Controllers/V1/Extra/Stats/RecentlyUpdatedItemsController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Universalis.DbAccess.Queries.MarketBoard;
+using Universalis.DbAccess.Uploads;
+
+namespace Universalis.Application.Controllers.V1.Extra.Stats
+{
+    [ApiController]
+    [Route("api/extra/stats/recently-updated")]
+    public class RecentlyUpdatedItemsController : ControllerBase
+    {
+        private readonly IRecentlyUpdatedItemsDbAccess _recentlyUpdatedItemsDb;
+
+        public RecentlyUpdatedItemsController(IRecentlyUpdatedItemsDbAccess recentlyUpdatedItemsDb)
+        {
+            _recentlyUpdatedItemsDb = recentlyUpdatedItemsDb;
+        }
+
+        public async Task<IEnumerable<uint>> Get()
+        {
+            return (await _recentlyUpdatedItemsDb.Retrieve(new RecentlyUpdatedItemsQuery())).Items;
+        }
+    }
+}
