096f61fff43a1f5dfa22772d55fd5e45e1c6a804
karashiiro <49822414+karashiiro@users.noreply.github.com>
2021-07-29 14:28:58 -0700

    Implement main architecture tests

--- src/Universalis.Application.Tests/ArchTests.cs
+++ src/Universalis.Application.Tests/ArchTests.cs
@@ -1,4 +1,7 @@
 using NetArchTest.Rules;
+using System;
+using Universalis.Application.Uploads.Behaviors;
+using Universalis.Application.Views;
 using Xunit;
 using Xunit.Abstractions;
 
@@ -6,5 +9,52 @@ namespace Universalis.Application.Tests
 {
     public class ArchTests
     {
+        private readonly ITestOutputHelper _output;
+
+        public ArchTests(ITestOutputHelper output)
+        {
+            _output = output;
+        }
+
+        [Fact]
+        public void Views_AreSiloed()
+        {
+            var result = Types.InAssembly(typeof(ListingView).Assembly)
+                .That()
+                .ResideInNamespace("Universalis.Application.Views")
+                .Should()
+                .NotHaveDependencyOnAny("Universalis.DbAccess", "Universalis.Entities")
+                .GetResult();
+            _output.WriteLine(string.Join(',', result.FailingTypeNames ?? Array.Empty<string>()));
+            Assert.True(result.IsSuccessful, "API views should not rely on database models.");
+        }
+
+        [Fact]
+        public void UploadSchema_AreSiloed()
+        {
+            var result = Types.InAssembly(typeof(ListingView).Assembly)
+                .That()
+                .ResideInNamespace("Universalis.Application.Uploads.Schema")
+                .Should()
+                .NotHaveDependencyOnAny("Universalis.DbAccess", "Universalis.Entities")
+                .GetResult();
+            _output.WriteLine(string.Join(',', result.FailingTypeNames ?? Array.Empty<string>()));
+            Assert.True(result.IsSuccessful, "API views should not rely on database models.");
+        }
+
+        [Fact]
+        public void UploadBehaviors_ImplementInterface()
+        {
+            var result = Types.InAssembly(typeof(IUploadBehavior).Assembly)
+                .That()
+                .ResideInNamespace("Universalis.Application.Uploads.Behaviors")
+                .And()
+                .AreClasses()
+                .Should()
+                .ImplementInterface(typeof(IUploadBehavior))
+                .GetResult();
+            _output.WriteLine(string.Join(',', result.FailingTypeNames ?? Array.Empty<string>()));
+            Assert.True(result.IsSuccessful);
+        }
     }
 }
