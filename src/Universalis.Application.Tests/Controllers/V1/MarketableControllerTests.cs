f88d271c0b9e6dfc34eb9766761fb25f57867515
karashiiro <49822414+karashiiro@users.noreply.github.com>
2021-07-29 15:02:19 -0700

    Implement marketable items endpoint tests

---
+++ src/Universalis.Application.Tests/Controllers/V1/MarketableControllerTests.cs
@@ -0,0 +1,18 @@
+﻿using Universalis.Application.Controllers.V1;
+using Universalis.Application.Tests.Mocks.GameData;
+using Xunit;
+
+namespace Universalis.Application.Tests.Controllers.V1
+{
+    public class MarketableControllerTests
+    {
+        [Fact]
+        public void Controller_Get_Succeeds()
+        {
+            var gameData = new MockGameDataProvider();
+            var controller = new MarketableController(gameData);
+            var result = controller.Get();
+            Assert.Equal(gameData.MarketableItemIds(), result);
+        }
+    }
+}
