fe9ba0eda70529e279bf9196b93fe881810e7d2a
karashiiro <49822414+karashiiro@users.noreply.github.com>
2021-07-30 00:40:43 -0700

    Implement content controller tests

---
+++ src/Universalis.Application.Tests/Controllers/V1/Extra/ContentControllerTests.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+using Universalis.Application.Controllers.V1.Extra;
+using Universalis.Application.Tests.Mocks.DbAccess;
+using Universalis.Application.Views;
+using Universalis.Entities;
+using Xunit;
+
+namespace Universalis.Application.Tests.Controllers.V1.Extra
+{
+    public class ContentControllerTests
+    {
+        [Fact]
+        public async Task Controller_Get_Succeeds()
+        {
+            var dbAccess = new MockContentDbAccess();
+            var controller = new ContentController(dbAccess);
+
+            var document = new Content
+            {
+                ContentId = "2A",
+                ContentType = ContentKind.Player,
+                CharacterName = "B B",
+            };
+
+            await dbAccess.Create(document);
+
+            var result = await controller.Get("2A");
+            var content = (ContentView)Assert.IsType<OkObjectResult>(result).Value;
+
+            Assert.Equal(document.ContentId, content.ContentId);
+            Assert.Equal(document.ContentType, content.ContentType);
+            Assert.Equal(document.CharacterName, content.CharacterName);
+        }
+    }
+}
---
+++ src/Universalis.Application.Tests/Mocks/DbAccess/MockContentDbAccess.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using Universalis.DbAccess;
+using Universalis.DbAccess.Queries;
+using Universalis.Entities;
+
+namespace Universalis.Application.Tests.Mocks.DbAccess
+{
+    public class MockContentDbAccess : IContentDbAccess
+    {
+        private readonly Dictionary<string, Content> _collection = new();
+
+        public Task Create(Content document)
+        {
+            _collection.Add(document.ContentId, document);
+            return Task.CompletedTask;
+        }
+
+        public Task<Content> Retrieve(ContentQuery query)
+        {
+            return !_collection.TryGetValue(query.ContentId, out var taxRates)
+                ? Task.FromResult<Content>(null)
+                : Task.FromResult(taxRates);
+        }
+
+        public async Task Update(Content document, ContentQuery query)
+        {
+            await Delete(query);
+            await Create(document);
+        }
+
+        public Task Delete(ContentQuery query)
+        {
+            _collection.Remove(query.ContentId);
+            return Task.CompletedTask;
+        }
+    }
+}
