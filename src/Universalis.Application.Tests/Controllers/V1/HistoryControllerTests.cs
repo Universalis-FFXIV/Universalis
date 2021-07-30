bf530bc029700d5b21d2eac0c25238aa22eda9ed
karashiiro <49822414+karashiiro@users.noreply.github.com>
2021-07-30 14:45:28 -0700

    Implement multi-item history tests for worlds

--- src/Universalis.Application.Tests/Controllers/V1/HistoryControllerTests.cs
+++ src/Universalis.Application.Tests/Controllers/V1/HistoryControllerTests.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections;
+using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
@@ -8,6 +10,7 @@ using Universalis.Application.Tests.Mocks.GameData;
 using Universalis.Application.Views;
 using Universalis.DataTransformations;
 using Universalis.Entities.MarketBoard;
+using Universalis.GameData;
 using Xunit;
 
 namespace Universalis.Application.Tests.Controllers.V1
@@ -46,48 +49,69 @@ namespace Universalis.Application.Tests.Controllers.V1
             var result = await controller.Get("5333", worldOrDc, entriesToReturn);
             var history = (HistoryView)Assert.IsType<OkObjectResult>(result).Value;
 
-            document.Sales.Sort((a, b) => (int)b.SaleTimeUnixSeconds - (int)a.SaleTimeUnixSeconds);
-            if (int.TryParse(entriesToReturn, out var entries))
+            AssertHistoryValidWorld(document, history, gameData, entriesToReturn);
+        }
+
+        [Theory]
+        [InlineData("74", "")]
+        [InlineData("Coeurl", " bingus4645")]
+        [InlineData("coEUrl", "50")]
+        public async Task Controller_Get_Succeeds_MultiItem_World(string worldOrDc, string entriesToReturn)
+        {
+            var gameData = new MockGameDataProvider();
+            var dbAccess = new MockHistoryDbAccess();
+            var controller = new HistoryController(gameData, dbAccess);
+            var rand = new Random();
+
+            var document1 = new History
             {
-                entries = Math.Max(0, entries);
-                Assert.True(history.Sales.Count <= entries);
-                document.Sales = document.Sales.Take(entries).ToList();
-            }
+                WorldId = 74,
+                ItemId = 5333,
+                LastUploadTimeUnixMilliseconds = (uint)DateTimeOffset.Now.ToUnixTimeMilliseconds(),
+                Sales = Enumerable.Range(0, 100)
+                    .Select(i => new MinimizedSale
+                    {
+                        Hq = rand.NextDouble() > 0.5,
+                        PricePerUnit = (uint)rand.Next(100, 60000),
+                        Quantity = (uint)rand.Next(1, 999),
+                        SaleTimeUnixSeconds = (uint)DateTimeOffset.Now.ToUnixTimeSeconds() - (uint)rand.Next(0, 80000),
+                        UploaderIdHash = "2A",
+                    })
+                    .ToList(),
+            };
+            await dbAccess.Create(document1);
 
-            var nqSales = document.Sales.Where(s => !s.Hq).ToList();
-            var hqSales = document.Sales.Where(s => s.Hq).ToList();
+            var document2 = new History
+            {
+                WorldId = 74,
+                ItemId = 5,
+                LastUploadTimeUnixMilliseconds = (uint)DateTimeOffset.Now.ToUnixTimeMilliseconds(),
+                Sales = Enumerable.Range(0, 100)
+                    .Select(i => new MinimizedSale
+                    {
+                        Hq = rand.NextDouble() > 0.5,
+                        PricePerUnit = (uint)rand.Next(100, 60000),
+                        Quantity = (uint)rand.Next(1, 999),
+                        SaleTimeUnixSeconds = (uint)DateTimeOffset.Now.ToUnixTimeSeconds() - (uint)rand.Next(0, 80000),
+                        UploaderIdHash = "2A",
+                    })
+                    .ToList(),
+            };
+            await dbAccess.Create(document2);
 
-            Assert.Equal(document.ItemId, history.ItemId);
-            Assert.Equal(document.WorldId, history.WorldId);
-            Assert.Equal(gameData.AvailableWorlds()[document.WorldId], history.WorldName);
+            var result = await controller.Get("5,5333", worldOrDc, entriesToReturn);
+            var history = (HistoryMultiView)Assert.IsType<OkObjectResult>(result).Value;
+
+            Assert.Contains(5U, history.ItemIds);
+            Assert.Contains(5333U, history.ItemIds);
+            Assert.Empty(history.UnresolvedItemIds);
+            Assert.Equal(2, history.Items.Count);
+            Assert.Equal(74U, history.WorldId);
+            Assert.Equal(gameData.AvailableWorlds()[74], history.WorldName);
             Assert.Null(history.DcName);
-            Assert.NotNull(history.Sales);
-            Assert.Equal(document.LastUploadTimeUnixMilliseconds, history.LastUploadTimeUnixMilliseconds);
 
-            Assert.Equal(Statistics.GetDistribution(document.Sales
-                        .Select(s => s.Quantity)
-                        .Select(q => (int)q))
-                    .ToDictionary(kvp => kvp.Key, kvp => kvp.Value),
-                history.StackSizeHistogram);
-            Assert.Equal(Statistics.GetDistribution(nqSales
-                        .Select(s => s.Quantity)
-                        .Select(q => (int)q))
-                    .ToDictionary(kvp => kvp.Key, kvp => kvp.Value),
-                history.StackSizeHistogramNq);
-            Assert.Equal(Statistics.GetDistribution(hqSales
-                        .Select(s => s.Quantity)
-                        .Select(q => (int)q))
-                    .ToDictionary(kvp => kvp.Key, kvp => kvp.Value),
-                history.StackSizeHistogramHq);
-            Assert.Equal(Statistics.WeekVelocityPerDay(document.Sales
-                    .Select(s => (long)s.SaleTimeUnixSeconds * 1000)),
-                history.SaleVelocity);
-            Assert.Equal(Statistics.WeekVelocityPerDay(nqSales
-                    .Select(s => (long)s.SaleTimeUnixSeconds * 1000)),
-                history.SaleVelocityNq);
-            Assert.Equal(Statistics.WeekVelocityPerDay(hqSales
-                    .Select(s => (long)s.SaleTimeUnixSeconds * 1000)),
-                history.SaleVelocityHq);
+            AssertHistoryValidWorld(document1, history.Items.First(item => item.ItemId == document1.ItemId), gameData, entriesToReturn);
+            AssertHistoryValidWorld(document2, history.Items.First(item => item.ItemId == document2.ItemId), gameData, entriesToReturn);
         }
 
         [Theory]
@@ -144,51 +168,7 @@ namespace Universalis.Application.Tests.Controllers.V1
                 document1.LastUploadTimeUnixMilliseconds,
                 document2.LastUploadTimeUnixMilliseconds);
 
-            sales.Sort((a, b) => (int)b.SaleTimeUnixSeconds - (int)a.SaleTimeUnixSeconds);
-            if (int.TryParse(entriesToReturn, out var entries))
-            {
-                entries = Math.Max(0, entries);
-                Assert.True(history.Sales.Count <= entries);
-                sales = sales.Take(entries).ToList();
-            }
-
-            var nqSales = sales.Where(s => !s.Hq).ToList();
-            var hqSales = sales.Where(s => s.Hq).ToList();
-
-            Assert.Contains(history.Sales, s => s.WorldId == 74);
-            Assert.Contains(history.Sales, s => s.WorldId == 34);
-
-            Assert.Equal(document1.ItemId, history.ItemId);
-            Assert.Equal(char.ToUpperInvariant(worldOrDc[0]) + worldOrDc[1..].ToLowerInvariant(), history.DcName);
-            Assert.Null(history.WorldId);
-            Assert.Null(history.WorldName);
-            Assert.NotNull(history.Sales);
-            Assert.Equal(lastUploadTime, history.LastUploadTimeUnixMilliseconds);
-
-            Assert.Equal(Statistics.GetDistribution(sales
-                        .Select(s => s.Quantity)
-                        .Select(q => (int)q))
-                    .ToDictionary(kvp => kvp.Key, kvp => kvp.Value),
-                history.StackSizeHistogram);
-            Assert.Equal(Statistics.GetDistribution(nqSales
-                        .Select(s => s.Quantity)
-                        .Select(q => (int)q))
-                    .ToDictionary(kvp => kvp.Key, kvp => kvp.Value),
-                history.StackSizeHistogramNq);
-            Assert.Equal(Statistics.GetDistribution(hqSales
-                        .Select(s => s.Quantity)
-                        .Select(q => (int)q))
-                    .ToDictionary(kvp => kvp.Key, kvp => kvp.Value),
-                history.StackSizeHistogramHq);
-            Assert.Equal(Statistics.WeekVelocityPerDay(sales
-                    .Select(s => (long)s.SaleTimeUnixSeconds * 1000)),
-                history.SaleVelocity);
-            Assert.Equal(Statistics.WeekVelocityPerDay(nqSales
-                    .Select(s => (long)s.SaleTimeUnixSeconds * 1000)),
-                history.SaleVelocityNq);
-            Assert.Equal(Statistics.WeekVelocityPerDay(hqSales
-                    .Select(s => (long)s.SaleTimeUnixSeconds * 1000)),
-                history.SaleVelocityHq);
+            AssertHistoryValidDataCenter(document1, history, sales, lastUploadTime, worldOrDc, entriesToReturn);
         }
 
         [Theory]
@@ -362,6 +342,101 @@ namespace Universalis.Application.Tests.Controllers.V1
             Assert.Empty(history.Items);
             Assert.Equal("Crystal", history.DcName);
             Assert.Null(history.WorldId);
+        }
+
+        private static void AssertHistoryValidWorld(History document, HistoryView history, IGameDataProvider gameData, string entriesToReturn)
+        {
+            document.Sales.Sort((a, b) => (int)b.SaleTimeUnixSeconds - (int)a.SaleTimeUnixSeconds);
+            if (int.TryParse(entriesToReturn, out var entries))
+            {
+                entries = Math.Max(0, entries);
+                Assert.True(history.Sales.Count <= entries);
+                document.Sales = document.Sales.Take(entries).ToList();
+            }
+
+            var nqSales = document.Sales.Where(s => !s.Hq).ToList();
+            var hqSales = document.Sales.Where(s => s.Hq).ToList();
+
+            Assert.Equal(document.ItemId, history.ItemId);
+            Assert.Equal(document.WorldId, history.WorldId);
+            Assert.Equal(gameData.AvailableWorlds()[document.WorldId], history.WorldName);
+            Assert.Null(history.DcName);
+            Assert.NotNull(history.Sales);
+            Assert.Equal(document.LastUploadTimeUnixMilliseconds, history.LastUploadTimeUnixMilliseconds);
+
+            Assert.Equal(Statistics.GetDistribution(document.Sales
+                        .Select(s => s.Quantity)
+                        .Select(q => (int)q))
+                    .ToDictionary(kvp => kvp.Key, kvp => kvp.Value),
+                history.StackSizeHistogram);
+            Assert.Equal(Statistics.GetDistribution(nqSales
+                        .Select(s => s.Quantity)
+                        .Select(q => (int)q))
+                    .ToDictionary(kvp => kvp.Key, kvp => kvp.Value),
+                history.StackSizeHistogramNq);
+            Assert.Equal(Statistics.GetDistribution(hqSales
+                        .Select(s => s.Quantity)
+                        .Select(q => (int)q))
+                    .ToDictionary(kvp => kvp.Key, kvp => kvp.Value),
+                history.StackSizeHistogramHq);
+            Assert.Equal(Statistics.WeekVelocityPerDay(document.Sales
+                    .Select(s => (long)s.SaleTimeUnixSeconds * 1000)),
+                history.SaleVelocity);
+            Assert.Equal(Statistics.WeekVelocityPerDay(nqSales
+                    .Select(s => (long)s.SaleTimeUnixSeconds * 1000)),
+                history.SaleVelocityNq);
+            Assert.Equal(Statistics.WeekVelocityPerDay(hqSales
+                    .Select(s => (long)s.SaleTimeUnixSeconds * 1000)),
+                history.SaleVelocityHq);
+        }
+
+        private static void AssertHistoryValidDataCenter(History anyWorldDocument, HistoryView history, List<MinimizedSale> sales, uint lastUploadTime, string worldOrDc, string entriesToReturn)
+        {
+            sales.Sort((a, b) => (int)b.SaleTimeUnixSeconds - (int)a.SaleTimeUnixSeconds);
+            if (int.TryParse(entriesToReturn, out var entries))
+            {
+                entries = Math.Max(0, entries);
+                Assert.True(history.Sales.Count <= entries);
+                sales = sales.Take(entries).ToList();
+            }
+
+            var nqSales = sales.Where(s => !s.Hq).ToList();
+            var hqSales = sales.Where(s => s.Hq).ToList();
+
+            Assert.Contains(history.Sales, s => s.WorldId == 74);
+            Assert.Contains(history.Sales, s => s.WorldId == 34);
+
+            Assert.Equal(anyWorldDocument.ItemId, history.ItemId);
+            Assert.Equal(char.ToUpperInvariant(worldOrDc[0]) + worldOrDc[1..].ToLowerInvariant(), history.DcName);
+            Assert.Null(history.WorldId);
+            Assert.Null(history.WorldName);
+            Assert.NotNull(history.Sales);
+            Assert.Equal(lastUploadTime, history.LastUploadTimeUnixMilliseconds);
+
+            Assert.Equal(Statistics.GetDistribution(sales
+                        .Select(s => s.Quantity)
+                        .Select(q => (int)q))
+                    .ToDictionary(kvp => kvp.Key, kvp => kvp.Value),
+                history.StackSizeHistogram);
+            Assert.Equal(Statistics.GetDistribution(nqSales
+                        .Select(s => s.Quantity)
+                        .Select(q => (int)q))
+                    .ToDictionary(kvp => kvp.Key, kvp => kvp.Value),
+                history.StackSizeHistogramNq);
+            Assert.Equal(Statistics.GetDistribution(hqSales
+                        .Select(s => s.Quantity)
+                        .Select(q => (int)q))
+                    .ToDictionary(kvp => kvp.Key, kvp => kvp.Value),
+                history.StackSizeHistogramHq);
+            Assert.Equal(Statistics.WeekVelocityPerDay(sales
+                    .Select(s => (long)s.SaleTimeUnixSeconds * 1000)),
+                history.SaleVelocity);
+            Assert.Equal(Statistics.WeekVelocityPerDay(nqSales
+                    .Select(s => (long)s.SaleTimeUnixSeconds * 1000)),
+                history.SaleVelocityNq);
+            Assert.Equal(Statistics.WeekVelocityPerDay(hqSales
+                    .Select(s => (long)s.SaleTimeUnixSeconds * 1000)),
+                history.SaleVelocityHq);
         }
     }
 }
