deb0f6ff298fbe18fa429d8ad8de38857a2ffb87
karashiiro <49822414+karashiiro@users.noreply.github.com>
2021-07-31 08:45:14 -0700

    Add marketable check to item ID upload behavior

--- src/Universalis.Application/Uploads/Behaviors/ItemIdUploadBehavior.cs
+++ src/Universalis.Application/Uploads/Behaviors/ItemIdUploadBehavior.cs
@@ -3,15 +3,18 @@ using System.Threading.Tasks;
 using Universalis.Application.Uploads.Schema;
 using Universalis.DbAccess.Uploads;
 using Universalis.Entities.Uploads;
+using Universalis.GameData;
 
 namespace Universalis.Application.Uploads.Behaviors
 {
     public class ItemIdUploadBehavior : IUploadBehavior
     {
+        private readonly IGameDataProvider _gameData;
         private readonly IRecentlyUpdatedItemsDbAccess _recentlyUpdatedItemsDb;
 
-        public ItemIdUploadBehavior(IRecentlyUpdatedItemsDbAccess recentlyUpdatedItemsDb)
+        public ItemIdUploadBehavior(IGameDataProvider gameData, IRecentlyUpdatedItemsDbAccess recentlyUpdatedItemsDb)
         {
+            _gameData = gameData;
             _recentlyUpdatedItemsDb = recentlyUpdatedItemsDb;
         }
 
@@ -23,6 +26,11 @@ namespace Universalis.Application.Uploads.Behaviors
         public async Task<IActionResult> Execute(TrustedSource source, UploadParameters parameters)
         {
             // ReSharper disable once PossibleInvalidOperationException
+            if (!_gameData.MarketableItemIds().Contains(parameters.ItemId.Value))
+            {
+                return new NotFoundObjectResult(parameters.ItemId);
+            }
+
             await _recentlyUpdatedItemsDb.Push(parameters.ItemId.Value);
             return null;
         }
