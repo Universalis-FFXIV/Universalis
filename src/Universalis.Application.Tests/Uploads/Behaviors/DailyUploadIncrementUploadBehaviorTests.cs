7def86b7498c710cfeda10607ce5bc1ce34e6a4b
karashiiro <49822414+karashiiro@users.noreply.github.com>
2021-07-31 10:26:06 -0700

    Implement daily upload increment behavior tests

---
+++ src/Universalis.Application.Tests/Uploads/Behaviors/DailyUploadIncrementUploadBehaviorTests.cs
@@ -0,0 +1,32 @@
+﻿using System.Threading.Tasks;
+using Universalis.Application.Tests.Mocks.DbAccess.Uploads;
+using Universalis.Application.Uploads.Behaviors;
+using Universalis.Application.Uploads.Schema;
+using Universalis.DbAccess.Queries.Uploads;
+using Xunit;
+
+namespace Universalis.Application.Tests.Uploads.Behaviors
+{
+    public class DailyUploadIncrementUploadBehaviorTests
+    {
+        [Fact]
+        public async Task Behavior_Succeeds()
+        {
+            var dbAccess = new MockUploadCountHistoryDbAccess();
+            var behavior = new DailyUploadIncrementUploadBehavior(dbAccess);
+
+            var upload = new UploadParameters();
+            Assert.True(behavior.ShouldExecute(upload));
+
+            var result = await behavior.Execute(null, upload);
+            Assert.Null(result);
+
+            var data = await dbAccess.Retrieve(new UploadCountHistoryQuery());
+
+            Assert.NotNull(data);
+            Assert.Single(data.UploadCountByDay);
+            Assert.False(data.LastPush == 0);
+            Assert.Equal(1U, data.UploadCountByDay[0]);
+        }
+    }
+}
