985aadcd30d8dbb1a8d3480d7a691737bc3f4063
karashiiro <49822414+karashiiro@users.noreply.github.com>
2021-07-30 18:11:10 -0700

    Stub LRU and MRU endpoints

---
+++ src/Universalis.Application/Controllers/V1/Extra/Stats/LeastRecentlyUpdatedItemsController.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Universalis.Application.Controllers.V1.Extra.Stats
+{
+    [ApiController]
+    [Route("api/extra/stats/least-recently-updated")]
+    public class LeastRecentlyUpdatedItemsController
+    {
+    }
+}
---
+++ src/Universalis.Application/Controllers/V1/Extra/Stats/MostRecentlyUpdatedItemsController.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Universalis.Application.Controllers.V1.Extra.Stats
+{
+    [ApiController]
+    [Route("api/extra/stats/most-recently-updated")]
+    public class MostRecentlyUpdatedItemsController
+    {
+    }
+}
