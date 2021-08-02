cd00c7e6ac7efb9d909962eb9a50ba9b9bb0ae6c
karashiiro <49822414+karashiiro@users.noreply.github.com>
2021-08-01 21:16:34 -0700

    Implement validator test for upload controller

--- src/Universalis.Application.Tests/Controllers/V1/UploadControllerTests.cs
+++ src/Universalis.Application.Tests/Controllers/V1/UploadControllerTests.cs
@@ -1,11 +1,14 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using System;
+using System.Collections.Generic;
 using System.Linq;
 using System.Security.Cryptography;
 using System.Text;
 using System.Threading.Tasks;
 using Universalis.Application.Controllers.V1;
+using Universalis.Application.Tests.Mocks.DbAccess;
 using Universalis.Application.Tests.Mocks.DbAccess.Uploads;
+using Universalis.Application.Tests.Mocks.GameData;
 using Universalis.Application.Uploads.Behaviors;
 using Universalis.Application.Uploads.Schema;
 using Universalis.Entities.Uploads;
@@ -98,6 +101,55 @@ namespace Universalis.Application.Tests.Controllers.V1
 
             var result = await controller.Post(key, upload);
             Assert.IsType<OkObjectResult>(result);
+        }
+
+        [Fact]
+        public async Task Controller_Post_Validators_Run_First()
+        {
+            var flaggedUploaders = new MockFlaggedUploaderDbAccess();
+            var trustedSources = new MockTrustedSourceDbAccess();
+            var content = new MockContentDbAccess();
+            var gameData = new MockGameDataProvider();
+            var worldUploadCounts = new MockWorldUploadCountDbAccess();
+            var controller = new UploadController(
+                trustedSources,
+                flaggedUploaders,
+                new List<IUploadBehavior>
+                {
+                    new PlayerContentUploadBehavior(content),
+                    new WorldIdUploadBehavior(gameData, worldUploadCounts),
+                });
+
+            const string key = "blah";
+            using (var sha256 = SHA256.Create())
+            {
+                await trustedSources.Create(new TrustedSource
+                {
+                    ApiKeySha256 = BitConverter.ToString(sha256.ComputeHash(Encoding.UTF8.GetBytes(key))),
+                });
+            }
+
+            var upload1 = new UploadParameters
+            {
+                WorldId = 0,
+                ContentId = "afffff",
+                CharacterName = "Bubu Bubba",
+                UploaderId = "affffe",
+            };
+
+            var result1 = await controller.Post(key, upload1);
+            Assert.IsType<NotFoundObjectResult>(result1);
+
+            var upload2 = new UploadParameters
+            {
+                WorldId = 74,
+                ContentId = "afffff",
+                CharacterName = "Bubu Bubba",
+                UploaderId = "affffe",
+            };
+
+            var result2 = await controller.Post(key, upload2);
+            Assert.IsType<OkObjectResult>(result2);
         }
     }
 }
