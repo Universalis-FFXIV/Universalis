fb9765d1461aaa75c6366746bc734ec13c7c2072
karashiiro <49822414+karashiiro@users.noreply.github.com>
2021-08-02 07:30:12 -0700

    Remove debug logging

--- src/Universalis.GameData/GameDataExtensions.cs
+++ src/Universalis.GameData/GameDataExtensions.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using Microsoft.Extensions.Configuration;
+﻿using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace Universalis.GameData
@@ -8,7 +7,6 @@ namespace Universalis.GameData
     {
         public static void AddGameData(this IServiceCollection sc, IConfiguration config)
         {
-            Console.WriteLine("Loading from {0}", config["GameData:SqPack"]);
             sc.AddSingleton(_ => new Lumina.GameData(config["GameData:SqPack"]));
             sc.AddSingleton<IGameDataProvider, GameDataProvider>();
         }
