bb297e9df5de51967ec7f30ae0bb833baaa848d4
karashiiro <49822414+karashiiro@users.noreply.github.com>
2021-08-13 14:14:18 -0700

    Add note on sale velocity in currently-shown docs

--- src/Universalis.Application/Views/CurrentlyShownView.cs
+++ src/Universalis.Application/Views/CurrentlyShownView.cs
@@ -66,18 +66,24 @@ namespace Universalis.Application.Views
 
         /// <summary>
         /// The average number of sales per day, over the past seven days (or the entirety of the shown sales, whichever comes first).
+        /// This number will tend to be the same for every item, because the number of shown sales is the same and over the same period.
+        /// This statistic is more useful in historical queries.
         /// </summary>
         [JsonProperty("regularSaleVelocity")]
         public float SaleVelocity { get; set; }
 
         /// <summary>
         /// The average number of NQ sales per day, over the past seven days (or the entirety of the shown sales, whichever comes first).
+        /// This number will tend to be the same for every item, because the number of shown sales is the same and over the same period.
+        /// This statistic is more useful in historical queries.
         /// </summary>
         [JsonProperty("nqSaleVelocity")]
         public float SaleVelocityNq { get; set; }
 
         /// <summary>
         /// The average number of HQ sales per day, over the past seven days (or the entirety of the shown sales, whichever comes first).
+        /// This number will tend to be the same for every item, because the number of shown sales is the same and over the same period.
+        /// This statistic is more useful in historical queries.
         /// </summary>
         [JsonProperty("hqSaleVelocity")]
         public float SaleVelocityHq { get; set; }
