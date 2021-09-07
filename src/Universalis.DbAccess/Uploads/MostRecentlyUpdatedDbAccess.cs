86f721ed8935a19508a1746c61e7156680d7155b
karashiiro <49822414+karashiiro@users.noreply.github.com>
2021-09-06 20:45:52 -0700

    Remove ARE

--- src/Universalis.DbAccess/Uploads/MostRecentlyUpdatedDbAccess.cs
+++ src/Universalis.DbAccess/Uploads/MostRecentlyUpdatedDbAccess.cs
@@ -11,54 +11,43 @@ namespace Universalis.DbAccess.Uploads
     public class MostRecentlyUpdatedDbAccess : DbAccessService<MostRecentlyUpdated, MostRecentlyUpdatedQuery>, IMostRecentlyUpdatedDbAccess
     {
         public static readonly int MaxItems = 200;
-
-        private readonly AutoResetEvent _reset = new(true);
-
+        
         public MostRecentlyUpdatedDbAccess(IMongoClient client) : base(client, Constants.DatabaseName, "mostRecentlyUpdated") { }
 
         public MostRecentlyUpdatedDbAccess(IMongoClient client, string databaseName) : base(client, databaseName, "mostRecentlyUpdated") { }
 
         public async Task Push(uint worldId, WorldItemUpload document, CancellationToken cancellationToken = default)
         {
-            _reset.WaitOne();
-            _reset.Reset();
-            try
+            var query = new MostRecentlyUpdatedQuery { WorldId = worldId };
+            var existing = await Retrieve(query, cancellationToken);
+
+            if (existing == null)
             {
-                var query = new MostRecentlyUpdatedQuery { WorldId = worldId };
-                var existing = await Retrieve(query, cancellationToken);
-
-                if (existing == null)
+                await Create(new MostRecentlyUpdated
                 {
-                    await Create(new MostRecentlyUpdated
-                    {
-                        WorldId = worldId,
-                        Uploads = new List<WorldItemUpload> { document },
-                    }, cancellationToken);
-                    return;
-                }
-
-                var uploads = existing.Uploads;
-                var existingIndex = uploads.FindIndex(o => o.ItemId == document.ItemId);
-                if (existingIndex != -1)
-                {
-                    uploads.RemoveAt(existingIndex);
-                    uploads.Insert(0, document);
-                }
-                else
-                {
-                    uploads.Insert(0, document);
-                    uploads = uploads.Take(MaxItems).ToList();
-                }
-
-                var updateBuilder = Builders<MostRecentlyUpdated>.Update;
-                var update = updateBuilder.Set(o => o.Uploads, uploads);
-                await Collection.UpdateOneAsync(query.ToFilterDefinition(), update,
-                    cancellationToken: cancellationToken);
+                    WorldId = worldId,
+                    Uploads = new List<WorldItemUpload> { document },
+                }, cancellationToken);
+                return;
             }
-            finally
+
+            var uploads = existing.Uploads;
+            var existingIndex = uploads.FindIndex(o => o.ItemId == document.ItemId);
+            if (existingIndex != -1)
             {
-                _reset.Set();
+                uploads.RemoveAt(existingIndex);
+                uploads.Insert(0, document);
             }
+            else
+            {
+                uploads.Insert(0, document);
+                uploads = uploads.Take(MaxItems).ToList();
+            }
+
+            var updateBuilder = Builders<MostRecentlyUpdated>.Update;
+            var update = updateBuilder.Set(o => o.Uploads, uploads);
+            await Collection.UpdateOneAsync(query.ToFilterDefinition(), update,
+                cancellationToken: cancellationToken);
         }
 
         public async Task<IList<MostRecentlyUpdated>> RetrieveMany(MostRecentlyUpdatedManyQuery query, CancellationToken cancellationToken = default)
