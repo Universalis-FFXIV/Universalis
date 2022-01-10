3d8d472464bd28fb81811572e3af8ffec8614da4
karashiiro <49822414+karashiiro@users.noreply.github.com>
2022-01-09 16:43:51 -0800

    Make currently-shown data query implement IEquatable<T>

--- src/Universalis.DbAccess/Queries/MarketBoard/CurrentlyShownQuery.cs
+++ src/Universalis.DbAccess/Queries/MarketBoard/CurrentlyShownQuery.cs
@@ -1,9 +1,10 @@
 ﻿using MongoDB.Driver;
+using System;
 using Universalis.Entities.MarketBoard;
 
 namespace Universalis.DbAccess.Queries.MarketBoard
 {
-    public class CurrentlyShownQuery : DbAccessQuery<CurrentlyShown>
+    public class CurrentlyShownQuery : DbAccessQuery<CurrentlyShown>, IEquatable<CurrentlyShownQuery>
     {
         public uint WorldId { get; init; }
 
@@ -14,6 +15,35 @@ namespace Universalis.DbAccess.Queries.MarketBoard
             var filterBuilder = Builders<CurrentlyShown>.Filter;
             var filter = filterBuilder.Eq(o => o.WorldId, WorldId) & filterBuilder.Eq(o => o.ItemId, ItemId);
             return filter;
+        }
+
+        public bool Equals(CurrentlyShownQuery other)
+        {
+            if (ReferenceEquals(null, other)) return false;
+            if (ReferenceEquals(this, other)) return true;
+            return WorldId == other.WorldId && ItemId == other.ItemId;
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (ReferenceEquals(null, obj)) return false;
+            if (ReferenceEquals(this, obj)) return true;
+            return obj.GetType() == GetType() && Equals((CurrentlyShownQuery)obj);
+        }
+
+        public override int GetHashCode()
+        {
+            return HashCode.Combine(WorldId, ItemId);
+        }
+
+        public static bool operator ==(CurrentlyShownQuery left, CurrentlyShownQuery right)
+        {
+            return Equals(left, right);
+        }
+
+        public static bool operator !=(CurrentlyShownQuery left, CurrentlyShownQuery right)
+        {
+            return !Equals(left, right);
         }
     }
 }
