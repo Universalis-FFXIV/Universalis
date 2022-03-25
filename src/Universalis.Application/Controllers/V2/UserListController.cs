4d5fdb1b6493fd1161614925e119260c2385d7e3
karashiiro <49822414+karashiiro@users.noreply.github.com>
2022-03-24 22:09:18 -0700

    Add user lists API endpoint

---
+++ src/Universalis.Application/Controllers/V2/UserListController.cs
@@ -0,0 +1,79 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using Universalis.Application.Swagger;
+using Universalis.Application.Views.V2;
+using Universalis.Mogboard;
+using Universalis.Mogboard.Entities;
+using Universalis.Mogboard.Entities.Id;
+
+namespace Universalis.Application.Controllers.V2;
+
+[ApiController]
+[ApiVersion("1")]
+[ApiVersion("2")]
+[Route("api")]
+[ApiExplorerSettings(IgnoreApi = true)]
+public class UserListController : ControllerBase
+{
+    private readonly IMogboardTable<UserList, UserListId> _userListTable;
+
+    public UserListController(IMogboardTable<UserList, UserListId> userListTable)
+    {
+        _userListTable = userListTable;
+    }
+
+    /// <summary>
+    /// Retrieves a user list.
+    /// </summary>
+    /// <param name="listId">The ID of the list to retrieve.</param>
+    /// <param name="cancellationToken"></param>
+    /// <response code="200">Data retrieved successfully.</response>
+    /// <response code="404">The list requested does not exist.</response>
+    [HttpGet]
+    [MapToApiVersion("1")]
+    [Route("lists/{listId:guid}")]
+    [ApiTag("User lists")]
+    [ProducesResponseType(typeof(UserListView), 200)]
+    [ProducesResponseType(404)]
+    public Task<IActionResult> Get(Guid listId, CancellationToken cancellationToken = default)
+    {
+        return GetV2(listId, cancellationToken);
+    }
+
+    /// <summary>
+    /// Retrieves a user list.
+    /// </summary>
+    /// <param name="listId">The ID of the list to retrieve.</param>
+    /// <param name="cancellationToken"></param>
+    /// <response code="200">Data retrieved successfully.</response>
+    /// <response code="404">The list requested does not exist.</response>
+    [HttpGet]
+    [MapToApiVersion("2")]
+    [Route("v{version:apiVersion}/lists/{listId:guid}")]
+    [ApiTag("User lists")]
+    [ProducesResponseType(typeof(UserListView), 200)]
+    [ProducesResponseType(404)]
+    public async Task<IActionResult> GetV2(Guid listId, CancellationToken cancellationToken = default)
+    {
+        var userListId = new UserListId(listId);
+        var userList = await _userListTable.Get(userListId, cancellationToken);
+        if (userList == null)
+        {
+            return NotFound();
+        }
+
+        var userListView = new UserListView
+        {
+            CreatedTimestampMs = userList.Added.ToUnixTimeMilliseconds().ToString(),
+            UpdatedTimestampMs = userList.Updated.ToUnixTimeMilliseconds().ToString(),
+            Name = userList.Name ?? string.Empty,
+            Items = userList.Items?.ToList() ?? new List<int>(),
+        };
+
+        return Ok(userListView);
+    }
+}
---
+++ src/Universalis.Application/Views/V2/UserListView.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections.Generic;
+using System.Text.Json.Serialization;
+
+namespace Universalis.Application.Views.V2;
+
+public class UserListView
+{
+    /// <summary>
+    /// The time that this list was created, in milliseconds since the UNIX epoch.
+    /// </summary>
+    [JsonPropertyName("created")]
+    public string CreatedTimestampMs { get; set; }
+
+    /// <summary>
+    /// The time that this list was updated, in milliseconds since the UNIX epoch.
+    /// </summary>
+    [JsonPropertyName("updated")] 
+    public string UpdatedTimestampMs { get; set; }
+
+    /// <summary>
+    /// The name of this list.
+    /// </summary>
+    [JsonPropertyName("name")] 
+    public string Name { get; set; }
+
+    /// <summary>
+    /// The IDs of the list items.
+    /// </summary>
+    [JsonPropertyName("itemIDs")] 
+    public IList<int> Items { get; set; }
+}
