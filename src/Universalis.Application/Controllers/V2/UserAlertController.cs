6a801d415f7c8fa6f93ac4ed2d4c2e91ebfcbbe7
karashiiro <49822414+karashiiro@users.noreply.github.com>
2022-03-27 13:32:06 -0700

    Add alerts endpoint

---
+++ src/Universalis.Application/Controllers/V2/UserAlertController.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Universalis.Application.Swagger;
+using Universalis.Application.Views.V2;
+using Universalis.Mogboard;
+using Universalis.Mogboard.Entities;
+using Universalis.Mogboard.Entities.Id;
+
+namespace Universalis.Application.Controllers.V2;
+
+[ApiController]
+[ApiVersion("1")]
+[ApiVersion("2")]
+[Route("api")]
+[RequireMogboardAuthorization]
+#if !DEBUG
+[ApiExplorerSettings(IgnoreApi = true)]
+#endif
+public class UserAlertController : ControllerBase
+{
+    private readonly IMogboardTable<UserAlert, UserAlertId> _alerts;
+
+    public UserAlertController(IMogboardTable<UserAlert, UserAlertId> alerts)
+    {
+        _alerts = alerts;
+    }
+
+    /// <summary>
+    /// Retrieves an alert. Requires the session cookie to be set correctly.
+    /// </summary>
+    /// <response code="200">Data retrieved successfully.</response>
+    /// <response code="403">No session information was set, or the corresponding user was missing.</response>
+    /// <response code="404">The alert could not be found.</response>
+    [HttpGet]
+    [MapToApiVersion("1")]
+    [Route("alerts/{id:guid}")]
+    [ApiTag("User alerts")]
+    [ProducesResponseType(typeof(UserAlertView), 200)]
+    [ProducesResponseType(403)]
+    [ProducesResponseType(404)]
+    public Task<IActionResult> Get(Guid id, CancellationToken cancellationToken = default)
+    {
+        return GetV2(id, cancellationToken);
+    }
+
+    /// <summary>
+    /// Retrieves an alert. Requires the session cookie to be set correctly.
+    /// </summary>
+    /// <response code="200">Data retrieved successfully.</response>
+    /// <response code="403">No session information was set, or the corresponding user was missing.</response>
+    /// <response code="404">The alert could not be found.</response>
+    [HttpGet]
+    [MapToApiVersion("2")]
+    [Route("v{version:apiVersion}/alerts/{id:guid}")]
+    [ApiTag("User alerts")]
+    [ProducesResponseType(typeof(UserAlertView), 200)]
+    [ProducesResponseType(403)]
+    [ProducesResponseType(404)]
+    public async Task<IActionResult> GetV2(Guid id, CancellationToken cancellationToken = default)
+    {
+        var alertId = new UserAlertId(id);
+        var alert = await _alerts.Get(alertId, cancellationToken);
+        if (alert == null)
+        {
+            return NotFound();
+        }
+
+        var userAlertView = new UserAlertView
+        {
+            Id = alert.Id.ToString(),
+            ItemId = alert.ItemId,
+            CreatedTimestampMs = alert.Added.ToUnixTimeMilliseconds().ToString(),
+            LastCheckedTimestampMs = alert.Added.ToUnixTimeMilliseconds().ToString(),
+            Name = alert.Name,
+            Server = alert.Server,
+            ExpiryTimestampMs = alert.Expiry.ToUnixTimeMilliseconds().ToString(),
+            TriggerConditions = alert.TriggerConditions!.ToArray(),
+            TriggerType = alert.TriggerType,
+            TriggerLastSentTimestampMs = alert.TriggerLastSent.ToUnixTimeMilliseconds().ToString(),
+            TriggerDataCenter = alert.TriggerDataCenter,
+            TriggerNq = alert.TriggerNq,
+            TriggerHq = alert.TriggerHq,
+            TriggerActive = alert.TriggerActive,
+        };
+
+        return Ok(userAlertView);
+    }
+}
---
+++ src/Universalis.Application/Views/V2/UserAlertView.cs
@@ -0,0 +1,90 @@
+﻿using System.Text.Json.Serialization;
+
+namespace Universalis.Application.Views.V2;
+
+public class UserAlertView
+{
+    /// <summary>
+    /// The alert's ID.
+    /// </summary>
+    [JsonPropertyName("id")]
+    public string Id { get; set; }
+
+    /// <summary>
+    /// The alert's item ID.
+    /// </summary>
+    [JsonPropertyName("itemID")]
+    public int ItemId { get; set; }
+
+    /// <summary>
+    /// The time that this alert was created, in milliseconds since the UNIX epoch.
+    /// </summary>
+    [JsonPropertyName("created")]
+    public string CreatedTimestampMs { get; set; }
+
+    /// <summary>
+    /// The last time that this alert was checked, in milliseconds since the UNIX epoch.
+    /// </summary>
+    [JsonPropertyName("lastChecked")]
+    public string LastCheckedTimestampMs { get; set; }
+
+    /// <summary>
+    /// The alert's name.
+    /// </summary>
+    [JsonPropertyName("name")]
+    public string Name { get; set; }
+
+    /// <summary>
+    /// The alert's server.
+    /// </summary>
+    [JsonPropertyName("server")]
+    public string Server { get; set; }
+
+    /// <summary>
+    /// The expiry time of this alert, in milliseconds since the UNIX epoch.
+    /// </summary>
+    [JsonPropertyName("expiry")]
+    public string ExpiryTimestampMs { get; set; }
+
+    /// <summary>
+    /// The trigger conditions for this alert.
+    /// </summary>
+    [JsonPropertyName("triggerConditions")]
+    public string[] TriggerConditions { get; set; }
+
+    /// <summary>
+    /// The trigger type of this alert.
+    /// </summary>
+    [JsonPropertyName("triggerType")]
+    public string TriggerType { get; set; }
+
+    /// <summary>
+    /// The last time this alert was triggered, in milliseconds since the UNIX epoch.
+    /// </summary>
+    [JsonPropertyName("triggerLastSent")]
+    public string TriggerLastSentTimestampMs { get; set; }
+
+    /// <summary>
+    /// Whether or not this alert should trigger on the entire data center.
+    /// </summary>
+    [JsonPropertyName("triggerDataCenter")]
+    public bool TriggerDataCenter { get; set; }
+
+    /// <summary>
+    /// Whether or not this alert should trigger on HQ items.
+    /// </summary>
+    [JsonPropertyName("triggerHQ")]
+    public bool TriggerHq { get; set; }
+
+    /// <summary>
+    /// Whether or not this alert should trigger on NQ items.
+    /// </summary>
+    [JsonPropertyName("triggerNQ")]
+    public bool TriggerNq { get; set; }
+
+    /// <summary>
+    /// Whether or not this alert is active.
+    /// </summary>
+    [JsonPropertyName("triggerActive")]
+    public bool TriggerActive { get; set; }
+}
