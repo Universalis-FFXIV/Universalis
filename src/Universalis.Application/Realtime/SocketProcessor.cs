59f3c1901773c7a1c21e3f63e925b77f5f97623e
karashiiro <49822414+karashiiro@users.noreply.github.com>
2022-04-19 10:17:51 -0700

    Increase WebSocket buffer size and add logging

--- src/Universalis.Application/Realtime/SocketProcessor.cs
+++ src/Universalis.Application/Realtime/SocketProcessor.cs
@@ -5,6 +5,7 @@ using System.Diagnostics;
 using System.Net.WebSockets;
 using System.Threading;
 using System.Threading.Tasks;
+using Microsoft.Extensions.Logging;
 using Universalis.Application.Realtime.Messages;
 
 namespace Universalis.Application.Realtime;
@@ -12,14 +13,16 @@ namespace Universalis.Application.Realtime;
 public class SocketProcessor : ISocketProcessor
 {
     private readonly ConcurrentDictionary<Guid, SocketClient> _connections;
+    private readonly ILogger<SocketProcessor> _logger;
 
     private static readonly Gauge WebSocketConnections = Metrics.CreateGauge("universalis_ws_connections", "WebSocket Connections");
     private static readonly Histogram MessageQueueTime = Metrics.CreateHistogram("universalis_ws_queue_milliseconds", "WebSocket Message Queue Milliseconds");
     private static readonly Counter MessagesSent = Metrics.CreateCounter("universalis_ws_sent", "WebSocket Messages Sent");
 
-    public SocketProcessor()
+    public SocketProcessor(ILogger<SocketProcessor> logger)
     {
         _connections = new ConcurrentDictionary<Guid, SocketClient>();
+        _logger = logger;
     }
 
     public void Publish(SocketMessage message)
@@ -41,7 +44,7 @@ public class SocketProcessor : ISocketProcessor
     {
         var id = Guid.NewGuid();
 
-        var conn = new SocketClient(ws, cs);
+        var conn = new SocketClient(ws, cs, new LoggerShield<SocketProcessor>(_logger, id));
         conn.OnClose += () =>
         {
             _connections.TryRemove(id, out _);
