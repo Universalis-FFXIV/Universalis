9e20cd5679447c841ee078d201fd39c2cf765955
karashiiro <49822414+karashiiro@users.noreply.github.com>
2022-04-20 22:25:11 -0700

    Add filters to EventCondition

--- src/Universalis.Application/Realtime/Messages/EventCondition.cs
+++ src/Universalis.Application/Realtime/Messages/EventCondition.cs
@@ -1,15 +1,20 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
+using System.Text.Json.Serialization;
 
 namespace Universalis.Application.Realtime.Messages;
 
 public class EventCondition : IEquatable<EventCondition>
 {
     private readonly string[] _channels;
+    private readonly IDictionary<string, string> _filters;
 
-    private EventCondition(string[] channels)
+    private EventCondition(string[] channels, IDictionary<string, string> filters)
     {
         _channels = channels;
+        _filters = filters;
     }
 
     public bool ShouldSend(SocketMessage message)
@@ -27,18 +32,55 @@ public class EventCondition : IEquatable<EventCondition>
             }
         }
 
+        if (_filters.Count > 0)
+        {
+            var properties = message.GetType()
+                .GetProperties()
+                .Where(prop => prop.GetGetMethod() != null)
+                .ToDictionary(prop => prop.GetCustomAttribute<JsonPropertyNameAttribute>()?.Name ?? prop.Name,
+                    prop => prop.GetGetMethod()?.Invoke(message, Array.Empty<object>())?.ToString());
+            foreach (var (key, val) in _filters)
+            {
+                if (properties[key] != val)
+                {
+                    return false;
+                }
+            }
+        }
+
         return true;
+    }
+
+    public static EventCondition Parse(string condition)
+    {
+        // Should be in a format like "listings/add{world=74, item=5}"
+        var channels = new string(condition.TakeWhile(c => c != '{').ToArray()).Trim().Split('/');
+
+        var filters = new Dictionary<string, string>();
+        var filtersStart = condition.IndexOf('{');
+        var filtersEnd = condition.IndexOf('}');
+        if (filtersStart != -1 && filtersEnd != -1)
+        {
+            var filtersStr = condition[filtersStart..filtersEnd];
+            filters = filtersStr
+                .Split(',')
+                .Select(s => s.Trim().Split('='))
+                .ToDictionary(p => p[0], p => p[1]);
+        }
+
+        return new EventCondition(channels, filters);
     }
 
     public bool Equals(EventCondition other)
     {
-        if (ReferenceEquals(null, other)) return false;
-        return ReferenceEquals(this, other) || Equals(_channels, other._channels);
+        if (other is null) return false;
+        if (ReferenceEquals(this, other)) return true;
+        return Equals(_channels, other._channels) && Equals(_filters, other._filters);
     }
 
     public override bool Equals(object obj)
     {
-        if (ReferenceEquals(null, obj)) return false;
+        if (obj is null) return false;
         if (ReferenceEquals(this, obj)) return true;
         return obj.GetType() == GetType() && Equals((EventCondition)obj);
     }
@@ -46,11 +88,5 @@ public class EventCondition : IEquatable<EventCondition>
     public override int GetHashCode()
     {
         return (_channels != null ? _channels.GetHashCode() : 0);
-    }
-
-    public static EventCondition Parse(string condition)
-    {
-        var channels = condition.Split('/');
-        return new EventCondition(channels);
     }
 }
