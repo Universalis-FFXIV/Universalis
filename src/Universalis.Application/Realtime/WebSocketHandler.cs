4d10fde62d3a1e14788c20ec4b4ad79c53ad344c
karashiiro <49822414+karashiiro@users.noreply.github.com>
2022-05-04 22:48:32 -0700

    Disable message compression because we're using BSON

--- src/Universalis.Application/Realtime/WebSocketHandler.cs
+++ src/Universalis.Application/Realtime/WebSocketHandler.cs
@@ -8,8 +8,7 @@ public static class WebSocketHandler
 {
     public static async Task Connect(HttpContext ctx, ISocketProcessor socketProcessor, CancellationToken cancellationToken = default)
     {
-        using var webSocket = await ctx.WebSockets.AcceptWebSocketAsync(
-            new WebSocketAcceptContext { DangerousEnableCompression = true });
+        using var webSocket = await ctx.WebSockets.AcceptWebSocketAsync(new WebSocketAcceptContext());
         var socketFinished = new TaskCompletionSource<object>(TaskCreationOptions.RunContinuationsAsynchronously);
 
         socketProcessor.AddSocket(webSocket, socketFinished, cancellationToken);
