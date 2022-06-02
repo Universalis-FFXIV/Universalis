f03415360d2a7ddfdae2854c4643b462c466f7d6
karashiiro <49822414+karashiiro@users.noreply.github.com>
2022-06-01 21:20:02 -0700

    Use a bodge cache on some of the extra data endpoints

--- src/Universalis.Application/Controllers/V1/Extra/Stats/WorldUploadCountController.cs
+++ src/Universalis.Application/Controllers/V1/Extra/Stats/WorldUploadCountController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading;
@@ -6,6 +7,7 @@ using System.Threading.Tasks;
 using Universalis.Application.Swagger;
 using Universalis.Application.Views.V1.Extra.Stats;
 using Universalis.DbAccess.Uploads;
+using Universalis.Entities.Uploads;
 
 namespace Universalis.Application.Controllers.V1.Extra.Stats;
 
@@ -15,6 +17,10 @@ namespace Universalis.Application.Controllers.V1.Extra.Stats;
 public class WorldUploadCountController : ControllerBase
 {
     private readonly IWorldUploadCountDbAccess _worldUploadCountDb;
+
+    // Bodge caching mechanism; TODO: fix
+    private static IList<WorldUploadCount> Data;
+    private static DateTime LastFetch = DateTime.Now;
 
     public WorldUploadCountController(IWorldUploadCountDbAccess worldUploadCountDb)
     {
@@ -29,12 +35,16 @@ public class WorldUploadCountController : ControllerBase
     [ProducesResponseType(typeof(IDictionary<string, WorldUploadCountView>), 200)]
     public async Task<IDictionary<string, WorldUploadCountView>> Get(CancellationToken cancellationToken = default)
     {
-        var data = (await _worldUploadCountDb.GetWorldUploadCounts(cancellationToken))
-            .Where(d => !string.IsNullOrEmpty(d.WorldName))
-            .ToList();
+        if (DateTime.Now - LastFetch < TimeSpan.FromMinutes(5))
+        {
+            Data = (await _worldUploadCountDb.GetWorldUploadCounts(cancellationToken))
+                .Where(d => !string.IsNullOrEmpty(d.WorldName))
+                .ToList();
+            LastFetch = DateTime.Now;
+        }
 
-        var sum = data.Sum(d => d.Count);
-        return data
+        var sum = Data.Sum(d => d.Count);
+        return Data
             .ToDictionary(d => d.WorldName, d => new WorldUploadCountView
             {
                 Count = d.Count,
