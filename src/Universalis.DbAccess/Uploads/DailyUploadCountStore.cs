093fe61986457fe07b2e108e482ae3896f6fccf8
karashiiro <49822414+karashiiro@users.noreply.github.com>
2022-06-02 22:54:27 -0700

    Add daily upload count store

---
+++ src/Universalis.DbAccess/Uploads/DailyUploadCountStore.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using StackExchange.Redis;
+
+namespace Universalis.DbAccess.Uploads;
+
+public class DailyUploadCountStore : IDailyUploadCountStore
+{
+    private readonly IConnectionMultiplexer _redis;
+
+    public DailyUploadCountStore(IConnectionMultiplexer redis)
+    {
+        _redis = redis;
+    }
+
+    public async Task Increment(string key, string lastPushKey)
+    {
+        var db = _redis.GetDatabase();
+        
+        var now = DateTimeOffset.Now.ToUnixTimeMilliseconds();
+        var lastPush = (long)await db.StringGetAsync(lastPushKey);
+
+        if (now - lastPush > 86400000)
+        {
+            await db.StringSetAsync(lastPushKey, now);
+            await db.ListLeftPushAsync(key, 0);
+        }
+
+        // This is probably not good
+        var count = (long)await db.ListGetByIndexAsync(key, 0);
+        count++;
+        await db.ListSetByIndexAsync(key, 0, count);
+    }
+
+    public async Task<IList<long>> GetUploadCounts(string key, int stop = -1)
+    {
+        var db = _redis.GetDatabase();
+        var counts = await db.ListRangeAsync(key, stop: stop);
+        return counts.Select(c => (long)c).ToList();
+    }
+}
---
+++ src/Universalis.DbAccess/Uploads/IDailyUploadCountStore.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Universalis.DbAccess.Uploads;
+
+public interface IDailyUploadCountStore
+{
+    Task Increment(string key, string lastPushKey);
+
+    Task<IList<long>> GetUploadCounts(string key, int stop = -1);
+}
