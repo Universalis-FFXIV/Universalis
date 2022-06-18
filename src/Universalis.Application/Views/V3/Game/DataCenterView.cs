b51ddec3914ec2b425b2392784615157515c1d4f
karashiiro <49822414+karashiiro@users.noreply.github.com>
2022-06-18 14:48:05 -0700

    Add v3 game data endpoints for items, worlds, and DCs

---
+++ src/Universalis.Application/Views/V3/Game/DataCenterView.cs
@@ -0,0 +1,12 @@
+﻿using System.Text.Json.Serialization;
+
+namespace Universalis.Application.Views.V3.Game;
+
+public class DataCenterView
+{
+    [JsonPropertyName("name")]
+    public string Name { get; init; }
+    
+    [JsonPropertyName("worlds")]
+    public uint[] Worlds { get; init; }
+}
---
+++ src/Universalis.Application/Views/V3/Game/WorldView.cs
@@ -0,0 +1,12 @@
+﻿using System.Text.Json.Serialization;
+
+namespace Universalis.Application.Views.V3.Game;
+
+public class WorldView
+{
+    [JsonPropertyName("id")]
+    public uint Id { get; init; }
+    
+    [JsonPropertyName("name")]
+    public string Name { get; init; }
+}
