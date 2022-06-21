a4450b0407326a64d4720147abf11483c0c21fc7
karashiiro <49822414+karashiiro@users.noreply.github.com>
2022-06-20 17:57:38 -0700

    Only return distinct time zones

--- src/Universalis.Application/Controllers/V3/Miscellaneous/TimeZoneController.cs
+++ src/Universalis.Application/Controllers/V3/Miscellaneous/TimeZoneController.cs
@@ -17,16 +17,37 @@ public class TimeZoneController
     /// </summary>
     [HttpGet]
     [MapToApiVersion("3")]
-    [ApiTag("Available data centers")]
+    [ApiTag("Available time zones")]
     [Route("v{version:apiVersion}/misc/time-zones")]
     [ProducesResponseType(typeof(IEnumerable<TimeZoneView>), 200)]
     public IEnumerable<TimeZoneView> Get()
     {
-        return TimeZoneInfo.GetSystemTimeZones().Where(tz => tz.HasIanaId).Select(tz => new TimeZoneView
+        var tzList = TimeZoneInfo.GetSystemTimeZones().Where(tz => tz.HasIanaId).ToArray();
+        var toReturn = new List<TimeZoneInfo>();
+        for (var i = 0; i < tzList.Length; i++)
         {
-            Id = tz.Id,
-            UtcOffset = tz.BaseUtcOffset.TotalHours,
-            FormattedName = tz.DisplayName,
-        });
+            var ok = true;
+            for (var j = 0; j < i; j++)
+            {
+                if (tzList[i].HasSameRules(tzList[j]))
+                {
+                    ok = false;
+                    break;
+                }
+            }
+
+            if (ok)
+            {
+                toReturn.Add(tzList[i]);
+            }
+        }
+        
+        return toReturn
+            .Select(tz => new TimeZoneView
+            {
+                Id = tz.Id,
+                UtcOffset = tz.BaseUtcOffset.TotalHours,
+                FormattedName = tz.DisplayName,
+            });
     }
 }
