58dbd45fee17851c9518293b7c639d3e05abaaae
karashiiro <49822414+karashiiro@users.noreply.github.com>
2022-06-20 17:18:37 -0700

    Add v3 time zones endpoint

---
+++ src/Universalis.Application/Controllers/V3/Miscellaneous/TimeZoneController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+using Universalis.Application.Swagger;
+using Universalis.Application.Views.V3.Miscellaneous;
+
+namespace Universalis.Application.Controllers.V3.Miscellaneous;
+
+[ApiController]
+[ApiVersion("3")]
+[Route("api")]
+public class TimeZoneController
+{
+    /// <summary>
+    /// Returns all installed time zones.
+    /// </summary>
+    [HttpGet]
+    [MapToApiVersion("3")]
+    [ApiTag("Available data centers")]
+    [Route("v{version:apiVersion}/misc/time-zones")]
+    [ProducesResponseType(typeof(IEnumerable<TimeZoneView>), 200)]
+    public IEnumerable<TimeZoneView> Get()
+    {
+        return TimeZoneInfo.GetSystemTimeZones().Where(tz => tz.HasIanaId).Select(tz => new TimeZoneView
+        {
+            Id = tz.Id,
+            UtcOffset = tz.BaseUtcOffset.TotalHours,
+            FormattedName = tz.DisplayName,
+        });
+    }
+}
---
+++ src/Universalis.Application/Views/V3/Miscellaneous/TimeZoneView.cs
@@ -0,0 +1,15 @@
+﻿using System.Text.Json.Serialization;
+
+namespace Universalis.Application.Views.V3.Miscellaneous;
+
+public class TimeZoneView
+{
+    [JsonPropertyName("id")]
+    public string Id { get; set; }
+    
+    [JsonPropertyName("offset")]
+    public double UtcOffset { get; set; }
+    
+    [JsonPropertyName("name")]
+    public string FormattedName { get; set; }
+}
