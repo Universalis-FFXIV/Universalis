8ae554d7ca88f5dca0b0b5a22fb84849cfe4c796
karashiiro <49822414+karashiiro@users.noreply.github.com>
2022-07-13 13:19:46 -0700

    Add Postgres exception filter

---
+++ src/Universalis.Application/ExceptionFilters/PostgresExceptionFilter.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+using Microsoft.Extensions.Logging;
+using Npgsql;
+
+namespace Universalis.Application.ExceptionFilters;
+
+public class PostgresExceptionFilter : IExceptionFilter
+{
+    private readonly ILogger _logger;
+
+    public PostgresExceptionFilter(ILoggerFactory loggerFactory)
+    {
+        _logger = loggerFactory.CreateLogger<PostgresExceptionFilter>();
+    }
+
+    public void OnException(ExceptionContext context)
+    {
+        if (context.Exception is not PostgresException postgresException) return;
+        _logger.LogInformation("Postgres driver raised an exception: {Message}\n\tException source: {File}:{Line}",
+            postgresException.MessageText, postgresException.File, postgresException.Line);
+        context.ExceptionHandled = true;
+        context.Result = new StatusCodeResult(500);
+    }
+}
