2bbb38d51c5f0f837b5f25b476259bc59611a850
karashiiro <49822414+karashiiro@users.noreply.github.com>
2022-10-05 22:47:23 -0700

    Add cache over tax rates

--- src/Universalis.Application.Tests/Mocks/DbAccess/MarketBoard/MockTaxRatesDbAccess.cs
+++ src/Universalis.Application.Tests/Mocks/DbAccess/MarketBoard/MockTaxRatesDbAccess.cs
@@ -11,15 +11,16 @@ public class MockTaxRatesDbAccess : ITaxRatesDbAccess
 {
     private readonly Dictionary<uint, TaxRates> _collection = new();
 
-    public Task<TaxRates> Retrieve(TaxRatesQuery query, CancellationToken cancellationToken = default)
+    public ValueTask<TaxRates> Retrieve(TaxRatesQuery query, CancellationToken cancellationToken = default)
     {
         return !_collection.TryGetValue(query.WorldId, out var taxRates)
-            ? Task.FromResult<TaxRates>(null)
-            : Task.FromResult(taxRates);
+            ? ValueTask.FromResult<TaxRates>(null)
+            : ValueTask.FromResult(taxRates);
     }
 
-    public async Task Update(TaxRates document, TaxRatesQuery query, CancellationToken cancellationToken = default)
+    public Task Update(TaxRates document, TaxRatesQuery query, CancellationToken cancellationToken = default)
     {
         _collection[query.WorldId] = document;
+        return Task.CompletedTask;
     }
 }
--- src/Universalis.Entities/MarketBoard/TaxRates.cs
+++ src/Universalis.Entities/MarketBoard/TaxRates.cs
@@ -1,6 +1,9 @@
-﻿namespace Universalis.Entities.MarketBoard;
+﻿using System;
+using Universalis.Common.Caching;
 
-public class TaxRates
+namespace Universalis.Entities.MarketBoard;
+
+public class TaxRates : IEquatable<TaxRates>, ICopyable
 {
     public int LimsaLominsa { get; init; }
 
@@ -15,6 +18,43 @@ public class TaxRates
     public int Crystarium { get; init; }
 
     public int OldSharlayan { get; init; }
-    
+
     public string UploadApplicationName { get; init; }
+
+    public ICopyable Clone()
+    {
+        return (ICopyable)MemberwiseClone();
+    }
+
+    public bool Equals(TaxRates other)
+    {
+        if (ReferenceEquals(null, other)) return false;
+        if (ReferenceEquals(this, other)) return true;
+        return LimsaLominsa == other.LimsaLominsa && Gridania == other.Gridania && Uldah == other.Uldah &&
+               Ishgard == other.Ishgard && Kugane == other.Kugane && Crystarium == other.Crystarium &&
+               OldSharlayan == other.OldSharlayan && UploadApplicationName == other.UploadApplicationName;
+    }
+
+    public override bool Equals(object obj)
+    {
+        if (ReferenceEquals(null, obj)) return false;
+        if (ReferenceEquals(this, obj)) return true;
+        return obj.GetType() == this.GetType() && Equals((TaxRates)obj);
+    }
+
+    public override int GetHashCode()
+    {
+        return HashCode.Combine(LimsaLominsa, Gridania, Uldah, Ishgard, Kugane, Crystarium, OldSharlayan,
+            UploadApplicationName);
+    }
+
+    public static bool operator ==(TaxRates left, TaxRates right)
+    {
+        return Equals(left, right);
+    }
+
+    public static bool operator !=(TaxRates left, TaxRates right)
+    {
+        return !Equals(left, right);
+    }
 }
