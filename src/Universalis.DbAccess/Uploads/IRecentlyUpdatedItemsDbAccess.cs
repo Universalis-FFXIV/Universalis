61fe8da1a1d5d23345096407cc278c2dcc97de19
karashiiro <49822414+karashiiro@users.noreply.github.com>
2022-10-06 20:24:00 -0700

    Cache recently updated items

--- src/Universalis.DbAccess/Uploads/IRecentlyUpdatedItemsDbAccess.cs
+++ src/Universalis.DbAccess/Uploads/IRecentlyUpdatedItemsDbAccess.cs
@@ -6,7 +6,7 @@ namespace Universalis.DbAccess.Uploads;
 
 public interface IRecentlyUpdatedItemsDbAccess
 {
-    public Task<RecentlyUpdatedItems> Retrieve(CancellationToken cancellationToken = default);
+    public ValueTask<RecentlyUpdatedItems> Retrieve(CancellationToken cancellationToken = default);
 
     public Task Push(uint itemId, CancellationToken cancellationToken = default);
 }
