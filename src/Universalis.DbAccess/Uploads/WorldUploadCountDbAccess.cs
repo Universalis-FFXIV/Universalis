7e4c6dc7c643a33a2374dfa6c9a25aee57163eea
Kara <49822414+karashiiro@users.noreply.github.com>
2022-10-07 16:21:02 -0700

    Cache world upload counts (#843)

--- src/Universalis.DbAccess/Uploads/WorldUploadCountDbAccess.cs
+++ src/Universalis.DbAccess/Uploads/WorldUploadCountDbAccess.cs
@@ -1,4 +1,6 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
 using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
@@ -7,29 +9,76 @@ using Universalis.Entities.Uploads;
 
 namespace Universalis.DbAccess.Uploads;
 
-public class WorldUploadCountDbAccess : IWorldUploadCountDbAccess
+public class WorldUploadCountDbAccess : IWorldUploadCountDbAccess, IDisposable
 {
     public static readonly string Key = "Universalis.WorldUploadCounts";
-    
+
     private readonly IWorldUploadCountStore _store;
+    private readonly ConcurrentDictionary<string, WorldUploadCount> _cache;
+    private readonly SemaphoreSlim _initLock;
+    private bool _initialized;
 
     public WorldUploadCountDbAccess(IWorldUploadCountStore store)
     {
         _store = store;
+        _cache = new ConcurrentDictionary<string, WorldUploadCount>();
+        _initLock = new SemaphoreSlim(1, 1);
+        _initialized = false;
     }
 
-    public Task Increment(WorldUploadCountQuery query, CancellationToken cancellationToken = default)
+    public async Task Increment(WorldUploadCountQuery query, CancellationToken cancellationToken = default)
     {
-        return _store.Increment(Key, query.WorldName);
+        await EnsureInitialized(cancellationToken);
+        if (_cache.TryGetValue(query.WorldName, out var worldUploadCount))
+        {
+            // Increments world upload count atomically
+            worldUploadCount.Increment();
+        }
+        else
+        {
+            // This is not atomic, but it is a rare case
+            _cache.TryAdd(query.WorldName, new WorldUploadCount { Count = 1, WorldName = query.WorldName });
+        }
+
+        await _store.Increment(Key, query.WorldName);
     }
 
     public async Task<IEnumerable<WorldUploadCount>> GetWorldUploadCounts(CancellationToken cancellationToken = default)
     {
-        var counts = await _store.GetWorldUploadCounts(Key);
-        return counts.Select(c => new WorldUploadCount
+        await EnsureInitialized(cancellationToken);
+        return _cache.Values.OrderByDescending(w => w.Count);
+    }
+
+    private ValueTask EnsureInitialized(CancellationToken cancellationToken = default)
+    {
+        return !_initialized ? new ValueTask(Initialize(cancellationToken)) : ValueTask.CompletedTask;
+    }
+
+    private async Task Initialize(CancellationToken cancellationToken = default)
+    {
+        await _initLock.WaitAsync(cancellationToken);
+        try
         {
-            WorldName = c.Key,
-            Count = c.Value,
-        });
+            if (!_initialized)
+            {
+                var items = await _store.GetWorldUploadCounts(Key);
+                foreach (var (world, count) in items)
+                {
+                    _cache[world] = new WorldUploadCount { Count = count, WorldName = world };
+                }
+
+                _initialized = true;
+            }
+        }
+        finally
+        {
+            _initLock.Release();
+        }
+    }
+
+    public void Dispose()
+    {
+        _initLock.Dispose();
+        GC.SuppressFinalize(this);
     }
 }
