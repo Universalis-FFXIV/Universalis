445f9ac32d23d2221ddebdf9732ff709fa189dc2
karashiiro <49822414+karashiiro@users.noreply.github.com>
2022-10-06 20:45:40 -0700

    Cache daily upload counts

--- src/Universalis.DbAccess/Uploads/IUploadCountHistoryDbAccess.cs
+++ src/Universalis.DbAccess/Uploads/IUploadCountHistoryDbAccess.cs
@@ -1,11 +1,12 @@
 ﻿using System.Collections.Generic;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace Universalis.DbAccess.Uploads;
 
 public interface IUploadCountHistoryDbAccess
 {
-    Task Increment();
+    Task Increment(CancellationToken cancellationToken = default);
 
-    Task<IList<long>> GetUploadCounts(int stop = -1);
+    ValueTask<IList<long>> GetUploadCounts(int stop = -1, CancellationToken cancellationToken = default);
 }
