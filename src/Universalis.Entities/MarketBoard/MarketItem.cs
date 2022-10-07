235c2e1b1e045169a88a311caf6ce935a86a6481
karashiiro <49822414+karashiiro@users.noreply.github.com>
2022-10-06 21:13:36 -0700

    Revert "Cache update times"
    This reverts commit b38920cf5e2e02e78a0161aa46dced87a19b6692.

--- src/Universalis.Entities/MarketBoard/MarketItem.cs
+++ src/Universalis.Entities/MarketBoard/MarketItem.cs
@@ -1,18 +1,12 @@
 ﻿using System;
-using Universalis.Common.Caching;
 
 namespace Universalis.Entities.MarketBoard;
 
-public class MarketItem : ICopyable
+public class MarketItem
 {
     public uint WorldId { get; init; }
-
+    
     public uint ItemId { get; init; }
 
     public DateTime LastUploadTime { get; set; }
-
-    public ICopyable Clone()
-    {
-        return (ICopyable)MemberwiseClone();
-    }
 }
