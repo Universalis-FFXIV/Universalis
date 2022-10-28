fdfe8a8f8a160ba54946d6007b27fed45ed7958a
Kara <49822414+karashiiro@users.noreply.github.com>
2022-10-28 07:41:27 -0700

    Add BsonIgnore to SerializableProperties (#852)

--- src/Universalis.Application/Common/PartiallySerializable.cs
+++ src/Universalis.Application/Common/PartiallySerializable.cs
@@ -1,10 +1,12 @@
 ﻿using System.Collections.Generic;
 using System.Text.Json.Serialization;
+using MongoDB.Bson.Serialization.Attributes;
 
 namespace Universalis.Application.Common; 
 
 public abstract class PartiallySerializable {
     
+    [BsonIgnore]
     [JsonIgnore] 
     public HashSet<string> SerializableProperties { get; set; }
 }
