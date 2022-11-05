db9da6b5a683b6bb6a3958c2e7285dfa1576b193
karashiiro <49822414+karashiiro@users.noreply.github.com>
2022-11-04 18:19:00 -0700

    Stop spamming logs for MQ

--- src/Universalis.Application/Realtime/Dispatchers/SalesAddDispatcher.cs
+++ src/Universalis.Application/Realtime/Dispatchers/SalesAddDispatcher.cs
@@ -1,5 +1,4 @@
 ﻿using MassTransit;
-using Microsoft.Extensions.Logging;
 using System.Threading.Tasks;
 using Universalis.Application.Realtime.Messages;
 
@@ -8,18 +7,15 @@ namespace Universalis.Application.Realtime.Dispatchers;
 public class SalesAddDispatcher : IConsumer<SalesAdd>
 {
     private readonly ISocketProcessor _sockets;
-    private readonly ILogger<SalesAddDispatcher> _logger;
 
-    public SalesAddDispatcher(ISocketProcessor sockets, ILogger<SalesAddDispatcher> logger)
+    public SalesAddDispatcher(ISocketProcessor sockets)
     {
         _sockets = sockets;
-        _logger = logger;
     }
 
     public Task Consume(ConsumeContext<SalesAdd> context)
     {
         _sockets.Publish(context.Message);
-        _logger.LogInformation("Published message of type {EventType}", context.Message.Event);
         return Task.CompletedTask;
     }
 }
