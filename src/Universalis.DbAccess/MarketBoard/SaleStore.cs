8e5901cc421b8168aaff2b87a056e2bfbaaeb42e
karashiiro <49822414+karashiiro@users.noreply.github.com>
2022-11-11 23:48:18 -0800

    Add caching back to sales

--- src/Universalis.DbAccess/MarketBoard/SaleStore.cs
+++ src/Universalis.DbAccess/MarketBoard/SaleStore.cs
@@ -1,20 +1,30 @@
-﻿using Npgsql;
+﻿using Microsoft.Extensions.Logging;
+using Npgsql;
+using StackExchange.Redis;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
 using Universalis.Entities.MarketBoard;
+using static Universalis.DbAccess.RedisDatabases;
 
 namespace Universalis.DbAccess.MarketBoard;
 
 public class SaleStore : ISaleStore
 {
-    private readonly string _connectionString;
+    // The maximum number of cached sales, per item, per world
+    private const int MaxCachedSales = 50;
 
-    public SaleStore(string connectionString)
+    private readonly string _connectionString;
+    private readonly ICacheRedisMultiplexer _cache;
+    private readonly ILogger<SaleStore> _logger;
+
+    public SaleStore(string connectionString, ICacheRedisMultiplexer cache, ILogger<SaleStore> logger)
     {
         _connectionString = connectionString;
+        _cache = cache;
+        _logger = logger;
     }
 
     public async Task Insert(Sale sale, CancellationToken cancellationToken = default)
@@ -44,6 +54,11 @@ public class SaleStore : ISaleStore
             },
         };
         await command.ExecuteNonQueryAsync(cancellationToken);
+
+        // Purge the cache
+        var cache = _cache.GetDatabase(RedisDatabases.Cache.Sales);
+        var cacheIndexKey = GetIndexCacheKey(sale.WorldId, sale.ItemId);
+        await cache.KeyDeleteAsync(cacheIndexKey, CommandFlags.FireAndForget);
     }
 
     public async Task InsertMany(IEnumerable<Sale> sales, CancellationToken cancellationToken = default)
@@ -56,8 +71,18 @@ public class SaleStore : ISaleStore
         await using var conn = new NpgsqlConnection(_connectionString);
         await conn.OpenAsync(cancellationToken);
         await using var batch = new NpgsqlBatch(conn);
+        var purgedCaches = new Dictionary<string, bool>();
         foreach (var sale in sales)
         {
+            // Purge the cache
+            var cache = _cache.GetDatabase(RedisDatabases.Cache.Sales);
+            var cacheIndexKey = GetIndexCacheKey(sale.WorldId, sale.ItemId);
+            if (!purgedCaches.TryGetValue(cacheIndexKey, out var purged) || !purged)
+            {
+                await cache.KeyDeleteAsync(cacheIndexKey, CommandFlags.FireAndForget);
+                purgedCaches[cacheIndexKey] = true;
+            }
+
             batch.BatchCommands.Add(new NpgsqlBatchCommand(
                 "INSERT INTO sale (id, world_id, item_id, hq, unit_price, quantity, buyer_name, sale_time, uploader_id, mannequin) VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10)")
             {
@@ -81,6 +106,18 @@ public class SaleStore : ISaleStore
 
     public async Task<IEnumerable<Sale>> RetrieveBySaleTime(uint worldId, uint itemId, int count, DateTime? from = null, CancellationToken cancellationToken = default)
     {
+        // Try retrieving data from the cache
+        var cache = _cache.GetDatabase(RedisDatabases.Cache.Sales);
+        var cacheIndexKey = GetIndexCacheKey(worldId, itemId);
+        if (from == null && count <= MaxCachedSales)
+        {
+            var cachedSales = await FetchSalesFromCache(cache, cacheIndexKey, worldId, itemId, count);
+            if (cachedSales.Any())
+            {
+                return cachedSales;
+            }
+        }
+
         // Fetch data from the database
         await using var conn = new NpgsqlConnection(_connectionString);
         await conn.OpenAsync(cancellationToken);
@@ -125,6 +162,136 @@ public class SaleStore : ISaleStore
             sales.Add(nextSale);
         }
 
-        return sales.Take(count).ToList();
+        var results = sales.Take(count).ToList();
+
+        // Store the results in the cache
+        if (results.Count >= MaxCachedSales)
+        {
+            await CacheSales(cache, cacheIndexKey, results);
+        }
+
+        return results;
+    }
+
+    private async Task<IList<Sale>> FetchSalesFromCache(IDatabase cache, string cacheIndexKey, uint worldId, uint itemId, int count)
+    {
+        try
+        {
+            if (await cache.KeyExistsAsync(cacheIndexKey))
+            {
+                var cachedSaleIds = await cache.ListRangeAsync(cacheIndexKey);
+                var cachedSaleTasks = cachedSaleIds
+                    .Take(count)
+                    .Select(async id =>
+                    {
+                        var saleId = Guid.Parse(id);
+                        var cacheKey = GetSaleCacheKey(saleId);
+                        return (saleId, await cache.HashGetAllAsync(cacheKey));
+                    });
+                return (await Task.WhenAll(cachedSaleTasks))
+                    .Select(cachedSale =>
+                    {
+                        var (saleId, sale) = cachedSale;
+                        return (saleId, sale.ToDictionary());
+                    })
+                    .Select(cachedSale =>
+                    {
+                        var (saleId, sale) = cachedSale;
+                        return new Sale
+                        {
+                            Id = saleId,
+                            WorldId = worldId,
+                            ItemId = itemId,
+                            Hq = GetValueBool(sale, "hq"),
+                            PricePerUnit = GetValueUInt32(sale, "ppu"),
+                            Quantity = GetValueUInt32(sale, "q"),
+                            BuyerName = GetValueString(sale, "bn"),
+                            SaleTime = DateTime.Parse(GetValueString(sale, "t")),
+                            UploaderIdHash = GetValueString(sale, "uid"),
+                            OnMannequin = GetValueBool(sale, "mann"),
+                        };
+                    })
+                    .OrderByDescending(sale => sale.SaleTime)
+                    .ToList();
+            }
+        }
+        catch (Exception e)
+        {
+            _logger.LogError(e, "Failed to retrieve sales from cache \"{SaleIndexCacheKey}\"", cacheIndexKey);
+        }
+
+        return new List<Sale>();
+    }
+
+    private async Task CacheSales(IDatabase cache, string cacheIndexKey, IEnumerable<Sale> sales)
+    {
+        var salesTrimmed = sales.Take(MaxCachedSales).ToList();
+        var saleIds = salesTrimmed.Select(sale => sale.Id.ToString());
+        try
+        {
+            // Add all of the sales
+            await Task.WhenAll(salesTrimmed.Select(async sale =>
+            {
+                var saleKey = GetSaleCacheKey(sale.Id);
+                await cache.HashSetAsync(saleKey, new[]
+                {
+                    new HashEntry("hq", sale.Hq),
+                    new HashEntry("ppu", sale.PricePerUnit),
+                    new HashEntry("q", sale.Quantity),
+                    new HashEntry("bn", sale.BuyerName),
+                    new HashEntry("t", sale.SaleTime.ToString()),
+                    new HashEntry("uid", sale.UploaderIdHash),
+                    new HashEntry("mann", sale.OnMannequin)
+                });
+                await cache.KeyExpireAsync(saleKey, TimeSpan.FromMinutes(5), flags: CommandFlags.FireAndForget);
+            }));
+
+            // Update the index
+            var s0 = await cache.ListGetByIndexAsync(cacheIndexKey, 0);
+            var tx = cache.CreateTransaction();
+            tx.AddCondition(Condition.ListIndexEqual(cacheIndexKey, 0, s0));
+            _ = tx.ListLeftPushAsync(cacheIndexKey, saleIds.Select(id => (RedisValue)id).ToArray());
+            _ = tx.ListTrimAsync(cacheIndexKey, 0, MaxCachedSales);
+            await tx.ExecuteAsync(CommandFlags.FireAndForget);
+        }
+        catch (Exception e)
+        {
+            _logger.LogError(e, "Failed to store sales in cache \"{SaleIndexCacheKey}\"", cacheIndexKey);
+        }
+    }
+
+    private static uint GetValueUInt32(IDictionary<RedisValue, RedisValue> hash, string key)
+    {
+        return hash.ContainsKey(key) ? (uint)hash[key] : 0;
+    }
+
+    private static int GetValueInt32(IDictionary<RedisValue, RedisValue> hash, string key)
+    {
+        return hash.ContainsKey(key) ? (int)hash[key] : 0;
+    }
+
+    private static long GetValueInt64(IDictionary<RedisValue, RedisValue> hash, string key)
+    {
+        return hash.ContainsKey(key) ? (long)hash[key] : 0;
+    }
+
+    private static bool GetValueBool(IDictionary<RedisValue, RedisValue> hash, string key)
+    {
+        return hash.ContainsKey(key) && (bool)hash[key];
+    }
+
+    private static string GetValueString(IDictionary<RedisValue, RedisValue> hash, string key)
+    {
+        return hash.ContainsKey(key) ? hash[key] : "";
+    }
+
+    private static string GetIndexCacheKey(uint worldId, uint itemId)
+    {
+        return $"sale-index:{worldId}:{itemId}";
+    }
+
+    private static string GetSaleCacheKey(Guid saleId)
+    {
+        return $"sale:{saleId}";
     }
 }
