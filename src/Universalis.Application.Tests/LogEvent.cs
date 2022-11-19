6f2bd57a8f749b9441665fadb40abc0e83c8d20f
karashiiro <49822414+karashiiro@users.noreply.github.com>
2022-11-19 13:52:31 -0800

    Add single listing deletions to WebSocket events

---
+++ src/Universalis.Application.Tests/LogEvent.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.Extensions.Logging;
+using System;
+
+namespace Universalis.Application.Tests;
+
+internal class LogEvent
+{
+    public LogLevel Level { get; set; }
+
+    public string Message { get; set; }
+
+    public Exception Exception { get; set; }
+
+    public object State { get; set; }
+}
---
+++ src/Universalis.Application.Tests/LogFixture.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+
+namespace Universalis.Application.Tests;
+
+internal class LogFixture<T> : ILogger<T>
+{
+    private readonly IList<LogEvent> _events = new List<LogEvent>();
+
+    public IDisposable BeginScope<TState>(TState state)
+    {
+        throw new NotImplementedException();
+    }
+
+    public bool IsEnabled(LogLevel logLevel)
+    {
+        return true;
+    }
+
+    public void Log<TState>(LogLevel logLevel, EventId eventId, TState state, Exception exception, Func<TState, Exception, string> formatter)
+    {
+        lock (_events)
+        {
+            _events.Add(new LogEvent
+            {
+                Level = logLevel,
+                Message = formatter(state, exception),
+                Exception = exception,
+                State = state,
+            });
+        }
+    }
+
+    internal class LogFixtureScope<TState> : IDisposable
+    {
+        public void Dispose()
+        {
+        }
+    }
+}
