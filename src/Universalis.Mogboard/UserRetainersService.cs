da4f8d22d3faa02d91ee5773b467d90873415fed
karashiiro <49822414+karashiiro@users.noreply.github.com>
2022-11-24 00:20:48 -0800

    Fix mogbaord database integrations

--- src/Universalis.Mogboard/UserRetainersService.cs
+++ src/Universalis.Mogboard/UserRetainersService.cs
@@ -6,22 +6,16 @@ namespace Universalis.Mogboard;
 
 internal class UserRetainersService : IMogboardTable<UserRetainer, UserRetainerId>
 {
-    private readonly string _username;
-    private readonly string _password;
-    private readonly string _database;
-    private readonly int _port;
+    private readonly string _connectionString;
 
-    public UserRetainersService(string username, string password, string database, int port)
+    public UserRetainersService(string connectionString)
     {
-        _username = username;
-        _password = password;
-        _database = database;
-        _port = port;
+        _connectionString = connectionString;
     }
 
     public async Task<UserRetainer?> Get(UserRetainerId id, CancellationToken cancellationToken = default)
     {
-        await using var db = new MySqlConnection($"User ID={_username};Password={_password};Database={_database};Server=localhost;Port={_port}");
+        await using var db = new MySqlConnection(_connectionString);
         await db.OpenAsync(cancellationToken);
 
         await using var command = db.CreateCommand();
@@ -35,7 +29,7 @@ internal class UserRetainersService : IMogboardTable<UserRetainer, UserRetainerI
 
     public async Task Create(UserRetainer entity, CancellationToken cancellationToken = default)
     {
-        await using var db = new MySqlConnection($"User ID={_username};Password={_password};Database={_database};Server=localhost;Port={_port}");
+        await using var db = new MySqlConnection(_connectionString);
         await db.OpenAsync(cancellationToken);
 
         await using var command = db.CreateCommand();
