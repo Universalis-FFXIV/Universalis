684b1ca072fc4c7879d762d74fc5405a732742cf
Kara <49822414+karashiiro@users.noreply.github.com>
2022-11-29 14:29:47 -0800

    Add the listings cache back again (#966)

--- src/Universalis.DbAccess/MarketBoard/CurrentlyShownStore.cs
+++ src/Universalis.DbAccess/MarketBoard/CurrentlyShownStore.cs
@@ -24,8 +24,20 @@ public class CurrentlyShownStore : ICurrentlyShownStore
 
     public async Task<CurrentlyShown> GetData(uint worldId, uint itemId)
     {
-        // This should not be cached. Caching this creates significant
-        // network pressure between the API and Redis. It's not worth it.
+        // Try to fetch data from the cache
+        var cache = _cache.GetDatabase(RedisDatabases.Cache.Listings);
+        try
+        {
+            var cachedObject = await FetchData(cache, worldId, itemId);
+            if (cachedObject != null)
+            {
+                return cachedObject;
+            }
+        }
+        catch (Exception e)
+        {
+            _logger.LogError(e, "Failed to retrieve currently shown data from the cache (world={WorldId}, item={ItemId})", worldId, itemId);
+        }
 
         // Fetch data from the database
         var db = _redis.GetDatabase(RedisDatabases.Instance0.CurrentData);
@@ -35,16 +47,21 @@ public class CurrentlyShownStore : ICurrentlyShownStore
             return new CurrentlyShown();
         }
 
+        // Store the result in the cache
+        await StoreData(cache, result, TimeSpan.FromSeconds(30));
+
         return result;
     }
 
     public async Task SetData(CurrentlyShown data)
     {
+        var cache = _cache.GetDatabase(RedisDatabases.Cache.Listings);
         var db = _redis.GetDatabase(RedisDatabases.Instance0.CurrentData);
         await StoreData(db, data);
+        await StoreData(cache, data, TimeSpan.FromSeconds(30));
     }
 
-    private async Task StoreData(IDatabase db, CurrentlyShown data, TimeSpan? expiry = null)
+    private static async Task StoreData(IDatabase db, CurrentlyShown data, TimeSpan? expiry = null)
     {
         var worldId = data.WorldId;
         var itemId = data.ItemId;
