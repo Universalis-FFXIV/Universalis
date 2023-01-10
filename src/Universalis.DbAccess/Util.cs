42a621500433099dd7f67b187738f160dc94c3b4
karashiiro <49822414+karashiiro@users.noreply.github.com>
2023-01-09 21:05:45 -0800

    Actually commit this file

---
+++ src/Universalis.DbAccess/Util.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace Universalis.DbAccess;
+
+public static class Util
+{
+    /// <summary>
+    /// Hashes the provided string.
+    /// </summary>
+    /// <param name="hasher">The hashing algorithm to use.</param>
+    /// <param name="input">The input string.</param>
+    /// <returns>A hash representing the input string.</returns>
+    public static string Hash(HashAlgorithm hasher, string input)
+    {
+        Span<byte> hash = stackalloc byte[hasher.HashSize/8];
+        ReadOnlySpan<byte> bytes = Encoding.UTF8.GetBytes(input ?? "");
+        if (hasher.TryComputeHash(bytes, hash, out var _written)) // Since we stackalloc the hash buffer, written is not needed
+            return Convert.ToHexString(hash).ToLowerInvariant(); // https://github.com/dotnet/runtime/issues/60393
+        throw new InvalidOperationException("Destination buffer was too small, this should never occur");
+    }
+}
