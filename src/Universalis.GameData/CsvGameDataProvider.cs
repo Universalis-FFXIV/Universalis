b8725b2dafa30c73706113fe3b86c46d4c517299
karashiiro <49822414+karashiiro@users.noreply.github.com>
2023-01-21 10:17:49 -0800

    Optimize game data loading
    This loads the CSV game data provider first, and loads Lumina in the background to use it once it's ready.
    CSV provider requests have been deduplicated.

--- src/Universalis.GameData/CsvGameDataProvider.cs
+++ src/Universalis.GameData/CsvGameDataProvider.cs
@@ -11,14 +11,14 @@ namespace Universalis.GameData;
 
 public class CsvGameDataProvider : IGameDataProvider
 {
-    private readonly IReadOnlyDictionary<int, string> _availableWorlds;
-    private readonly IReadOnlyDictionary<string, int> _availableWorldsReversed;
-    private readonly IReadOnlySet<int> _availableWorldIds;
+    private IReadOnlyDictionary<int, string> _availableWorlds;
+    private IReadOnlyDictionary<string, int> _availableWorldsReversed;
+    private IReadOnlySet<int> _availableWorldIds;
 
-    private readonly IReadOnlySet<int> _marketableItemIds;
-    private readonly IReadOnlyDictionary<int, int> _marketableItemStackSizes;
+    private IReadOnlySet<int> _marketableItemIds;
+    private IReadOnlyDictionary<int, int> _marketableItemStackSizes;
 
-    private readonly IReadOnlyList<DataCenter> _dataCenters;
+    private IReadOnlyList<DataCenter> _dataCenters;
 
     private readonly HttpClient _http;
 
@@ -26,14 +26,22 @@ public class CsvGameDataProvider : IGameDataProvider
     {
         _http = http;
 
-        _availableWorlds = LoadAvailableWorlds().GetAwaiter().GetResult();
-        _availableWorldsReversed = LoadAvailableWorldsReversed().GetAwaiter().GetResult();
-        _availableWorldIds = LoadAvailableWorldIds().GetAwaiter().GetResult();
+        // This is an anti-pattern but it only runs once so it's fine
+        Task.Run(async () =>
+        {
+            var worlds = await GetWorlds();
+            var items = await GetItems();
+            var dcs = await GetDataCenters();
 
-        _marketableItemIds = LoadMarketableItems().GetAwaiter().GetResult();
-        _marketableItemStackSizes = LoadMarketableItemStackSizes().GetAwaiter().GetResult();
+            _availableWorlds = await LoadAvailableWorlds(worlds);
+            _availableWorldsReversed = await LoadAvailableWorldsReversed(worlds);
+            _availableWorldIds = await LoadAvailableWorldIds(worlds);
 
-        _dataCenters = LoadDataCenters().GetAwaiter().GetResult();
+            _marketableItemIds = await LoadMarketableItems(items);
+            _marketableItemStackSizes = await LoadMarketableItemStackSizes(items);
+
+            _dataCenters = await LoadDataCenters(worlds, dcs);
+        }).GetAwaiter().GetResult();
     }
 
     IReadOnlyDictionary<int, string> IGameDataProvider.AvailableWorlds()
@@ -47,107 +55,94 @@ public class CsvGameDataProvider : IGameDataProvider
 
     IReadOnlySet<int> IGameDataProvider.MarketableItemIds()
         => _marketableItemIds;
-    
+
     IReadOnlyDictionary<int, int> IGameDataProvider.MarketableItemStackSizes()
         => _marketableItemStackSizes;
 
     IEnumerable<DataCenter> IGameDataProvider.DataCenters()
         => _dataCenters;
 
-    private async Task<IReadOnlyDictionary<int, string>> LoadAvailableWorlds()
+    private async Task<IList<CsvWorld>> GetWorlds()
     {
         var csvData =
-            await _http.GetStreamAsync("https://raw.githubusercontent.com/xivapi/ffxiv-datamining/master/csv/World.csv");
+            await _http.GetStreamAsync(
+                "https://raw.githubusercontent.com/xivapi/ffxiv-datamining/master/csv/World.csv");
         using var reader = new StreamReader(csvData);
         using var csv = new CsvReader(reader, CultureInfo.InvariantCulture);
         for (var i = 0; i < 3; i++) await csv.ReadAsync();
-        var worlds = csv.GetRecords<CsvWorld>();
-        return GetValidWorlds(worlds)
-            .Select(w => new World { Name = w.Name, Id = w.RowId })
-            .Concat(ChineseServers.Worlds())
-            .ToDictionary(w => w.Id, w => w.Name);
+        var worlds = csv.GetRecords<CsvWorld>().ToList();
+        return worlds;
     }
 
-    private async Task<IReadOnlyDictionary<string, int>> LoadAvailableWorldsReversed()
+    private async Task<IList<CsvItem>> GetItems()
     {
         var csvData =
-            await _http.GetStreamAsync("https://raw.githubusercontent.com/xivapi/ffxiv-datamining/master/csv/World.csv");
+            await _http.GetStreamAsync("https://raw.githubusercontent.com/xivapi/ffxiv-datamining/master/csv/Item.csv");
         using var reader = new StreamReader(csvData);
         using var csv = new CsvReader(reader, CultureInfo.InvariantCulture);
-        for (var i = 0; i < 3; i++) await csv.ReadAsync();
-        var worlds = csv.GetRecords<CsvWorld>();
-        return GetValidWorlds(worlds)
-            .Select(w => new World { Name = w.Name, Id = w.RowId })
-            .Concat(ChineseServers.Worlds())
-            .ToDictionary(w => w.Name, w => w.Id);
+        await csv.ReadAsync();
+        await csv.ReadAsync();
+        csv.ReadHeader();
+        await csv.ReadAsync();
+        var items = csv.GetRecords<CsvItem>().ToList();
+        return items;
     }
 
-    private async Task<IReadOnlySet<int>> LoadAvailableWorldIds()
+    private async Task<IList<CsvDc>> GetDataCenters()
     {
-        var csvData =
-            await _http.GetStreamAsync("https://raw.githubusercontent.com/xivapi/ffxiv-datamining/master/csv/World.csv");
-        using var reader = new StreamReader(csvData);
-        using var csv = new CsvReader(reader, CultureInfo.InvariantCulture);
-        for (var i = 0; i < 3; i++) await csv.ReadAsync();
-        var worlds = csv.GetRecords<CsvWorld>();
-        return new SortedSet<int>(GetValidWorlds(worlds)
+        var dcData =
+            await _http.GetStreamAsync(
+                "https://raw.githubusercontent.com/xivapi/ffxiv-datamining/master/csv/WorldDCGroupType.csv");
+        using var dcReader = new StreamReader(dcData);
+        using var dcCsv = new CsvReader(dcReader, CultureInfo.InvariantCulture);
+        for (var i = 0; i < 3; i++) await dcCsv.ReadAsync();
+        var dcs = dcCsv.GetRecords<CsvDc>().ToList();
+        return dcs;
+    }
+
+    private static Task<IReadOnlyDictionary<int, string>> LoadAvailableWorlds(IEnumerable<CsvWorld> worlds)
+    {
+        return Task.FromResult<IReadOnlyDictionary<int, string>>(GetValidWorlds(worlds)
+            .Select(w => new World { Name = w.Name, Id = w.RowId })
+            .Concat(ChineseServers.Worlds())
+            .ToDictionary(w => w.Id, w => w.Name));
+    }
+
+    private static Task<IReadOnlyDictionary<string, int>> LoadAvailableWorldsReversed(IEnumerable<CsvWorld> worlds)
+    {
+        return Task.FromResult<IReadOnlyDictionary<string, int>>(GetValidWorlds(worlds)
+            .Select(w => new World { Name = w.Name, Id = w.RowId })
+            .Concat(ChineseServers.Worlds())
+            .ToDictionary(w => w.Name, w => w.Id));
+    }
+
+    private static Task<IReadOnlySet<int>> LoadAvailableWorldIds(IEnumerable<CsvWorld> worlds)
+    {
+        return Task.FromResult<IReadOnlySet<int>>(new SortedSet<int>(GetValidWorlds(worlds)
             .Select(w => new World { Name = w.Name, Id = w.RowId })
             .Concat(ChineseServers.Worlds())
             .Select(w => w.Id)
-            .ToList());
+            .ToList()));
     }
 
-    private async Task<IReadOnlySet<int>> LoadMarketableItems()
+    private static Task<IReadOnlySet<int>> LoadMarketableItems(IEnumerable<CsvItem> items)
     {
-        var csvData =
-            await _http.GetStreamAsync("https://raw.githubusercontent.com/xivapi/ffxiv-datamining/master/csv/Item.csv");
-        using var reader = new StreamReader(csvData);
-        using var csv = new CsvReader(reader, CultureInfo.InvariantCulture);
-        await csv.ReadAsync();
-        await csv.ReadAsync();
-        csv.ReadHeader();
-        await csv.ReadAsync();
-        var items = csv.GetRecords<CsvItem>();
-        return new SortedSet<int>(items
+        return Task.FromResult<IReadOnlySet<int>>(new SortedSet<int>(items
             .Where(i => i.ItemSearchCategory >= 1)
             .Select(i => i.RowId)
-            .ToList());
+            .ToList()));
     }
-    
-    private async Task<IReadOnlyDictionary<int, int>> LoadMarketableItemStackSizes()
+
+    private static Task<IReadOnlyDictionary<int, int>> LoadMarketableItemStackSizes(IEnumerable<CsvItem> items)
     {
-        var csvData =
-            await _http.GetStreamAsync("https://raw.githubusercontent.com/xivapi/ffxiv-datamining/master/csv/Item.csv");
-        using var reader = new StreamReader(csvData);
-        using var csv = new CsvReader(reader, CultureInfo.InvariantCulture);
-        await csv.ReadAsync();
-        await csv.ReadAsync();
-        csv.ReadHeader();
-        await csv.ReadAsync();
-        var items = csv.GetRecords<CsvItem>();
-        return items
+        return Task.FromResult<IReadOnlyDictionary<int, int>>(items
             .Where(i => i.ItemSearchCategory >= 1)
-            .ToDictionary(i => i.RowId, i => i.StackSize);
+            .ToDictionary(i => i.RowId, i => i.StackSize));
     }
 
-    private async Task<IReadOnlyList<DataCenter>> LoadDataCenters()
+    private static Task<IReadOnlyList<DataCenter>> LoadDataCenters(IEnumerable<CsvWorld> worlds, IEnumerable<CsvDc> dcs)
     {
-        var worldData =
-            await _http.GetStreamAsync("https://raw.githubusercontent.com/xivapi/ffxiv-datamining/master/csv/World.csv");
-        using var worldReader = new StreamReader(worldData);
-        using var worldCsv = new CsvReader(worldReader, CultureInfo.InvariantCulture);
-
-        var dcData =
-            await _http.GetStreamAsync("https://raw.githubusercontent.com/xivapi/ffxiv-datamining/master/csv/WorldDCGroupType.csv");
-        using var dcReader = new StreamReader(dcData);
-        using var dcCsv = new CsvReader(dcReader, CultureInfo.InvariantCulture);
-
-        for (var i = 0; i < 3; i++) await worldCsv.ReadAsync();
-        for (var i = 0; i < 3; i++) await dcCsv.ReadAsync();
-
-        var worlds = worldCsv.GetRecords<CsvWorld>();
-        var dcs = dcCsv.GetRecords<CsvDc>();
-        return dcs
+        return Task.FromResult<IReadOnlyList<DataCenter>>(dcs
             .Where(dc => dc.RowId is > 0 and < 99)
             .Select(dc => new DataCenter
             {
@@ -160,7 +155,7 @@ public class CsvGameDataProvider : IGameDataProvider
             })
             .Where(dc => dc.WorldIds.Length > 0)
             .Concat(ChineseServers.DataCenters())
-            .ToList();
+            .ToList());
     }
 
     private static IEnumerable<CsvWorld> GetValidWorlds(IEnumerable<CsvWorld> worlds)
@@ -173,49 +168,36 @@ public class CsvGameDataProvider : IGameDataProvider
 
     private class CsvItem
     {
-        [Index(0)]
-        public int RowId { get; set; }
+        [Index(0)] public int RowId { get; set; }
 
-        [Name("ItemSearchCategory")]
-        public int ItemSearchCategory { get; set; }
-        
-        [Name("StackSize")]
-        public int StackSize { get; set; }
+        [Name("ItemSearchCategory")] public int ItemSearchCategory { get; set; }
+
+        [Name("StackSize")] public int StackSize { get; set; }
     }
 
     private class CsvWorld
     {
-        [Index(0)]
-        public int RowId { get; set; }
+        [Index(0)] public int RowId { get; set; }
 
-        [Index(1)]
-        public string InternalName { get; set; }
+        [Index(1)] public string InternalName { get; set; }
 
-        [Index(2)]
-        public string Name { get; set; }
+        [Index(2)] public string Name { get; set; }
 
-        [Index(3)]
-        public byte Region { get; set; }
+        [Index(3)] public byte Region { get; set; }
 
-        [Index(4)]
-        public byte UserType { get; set; }
+        [Index(4)] public byte UserType { get; set; }
 
-        [Index(5)]
-        public int DataCenter { get; set; }
+        [Index(5)] public int DataCenter { get; set; }
 
-        [Index(6)]
-        public bool IsPublic { get; set; }
+        [Index(6)] public bool IsPublic { get; set; }
     }
 
     private class CsvDc
     {
-        [Index(0)]
-        public uint RowId { get; set; }
+        [Index(0)] public uint RowId { get; set; }
 
-        [Index(1)]
-        public string Name { get; set; }
+        [Index(1)] public string Name { get; set; }
 
-        [Index(2)]
-        public byte Region { get; set; }
+        [Index(2)] public byte Region { get; set; }
     }
 }
