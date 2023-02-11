8ce6e4e22249da9c76842cee1b4c23816f3d6970
karashiiro <49822414+karashiiro@users.noreply.github.com>
2023-02-11 07:51:23 -0800

    Fix upload log SQL syntax
    This was only on canary so yolo

--- src/Universalis.DbAccess/Uploads/UploadLogStore.cs
+++ src/Universalis.DbAccess/Uploads/UploadLogStore.cs
@@ -18,7 +18,7 @@ public class UploadLogStore : IUploadLogStore
     {
         using var activity = Util.ActivitySource.StartActivity("UploadLogStore.LogAction");
         await using var command = _dataSource.CreateCommand(
-            "INSERT INTO upload_log (id, timestamp, event, application, world_id, item_id, listings, sales) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)");
+            "INSERT INTO upload_log (id, timestamp, event, application, world_id, item_id, listings, sales) VALUES (?, ?, ?, ?, ?, ?, ?, ?)");
         command.Parameters.Add(new NpgsqlParameter<Guid> { TypedValue = entry.Id });
         command.Parameters.Add(new NpgsqlParameter<DateTimeOffset> { TypedValue = entry.Timestamp });
         command.Parameters.Add(new NpgsqlParameter<string> { TypedValue = entry.Event });
