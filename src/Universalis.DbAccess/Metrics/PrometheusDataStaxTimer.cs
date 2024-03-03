cd531015decebdec2e0e129bf5a21ce5da09b6f5
karashiiro <49822414+karashiiro@users.noreply.github.com>
2024-03-03 14:20:43 -0800

    Set up metrics provider for DataStax Cassandra driver

---
+++ src/Universalis.DbAccess/Metrics/PrometheusDataStaxTimer.cs
@@ -0,0 +1,27 @@
+﻿using Cassandra.Metrics.Abstractions;
+using Prometheus;
+
+namespace Universalis.DbAccess.Metrics;
+
+public class PrometheusDataStaxTimer : IDriverTimer
+{
+    private readonly Histogram _histogram;
+
+    private readonly string _bucket;
+
+    public PrometheusDataStaxTimer(string name, string bucket)
+    {
+        _bucket = bucket;
+        _histogram = Prometheus.Metrics.CreateHistogram(name, "A timer measuring data in nanoseconds.",
+            new HistogramConfiguration
+            {
+                LabelNames = new[] { bucket },
+                Buckets = Histogram.ExponentialBuckets(1, 2, 16),
+            });
+    }
+
+    public void Record(long elapsedNanoseconds)
+    {
+        _histogram.WithLabels(_bucket).Observe(elapsedNanoseconds);
+    }
+}
