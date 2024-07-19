6d1229266466d33507f7eb10556deced1641445c
karashiiro <49822414+karashiiro@users.noreply.github.com>
2024-07-19 08:48:02 -0700

    Skip Redis for upload times in retrieve path

--- src/Universalis.DbAccess/MarketBoard/CurrentlyShownStore.cs
+++ src/Universalis.DbAccess/MarketBoard/CurrentlyShownStore.cs
@@ -61,12 +61,6 @@ public class CurrentlyShownStore : ICurrentlyShownStore
     {
         using var activity = Util.ActivitySource.StartActivity("CurrentlyShownStore.Retrieve");
 
-        var lastUpdated = await GetLastUpdated(query.WorldId, query.ItemId);
-        if (lastUpdated == 0)
-        {
-            return null;
-        }
-
         // Attempt to retrieve listings from Postgres
         List<Listing> listings;
         try
@@ -83,13 +77,14 @@ public class CurrentlyShownStore : ICurrentlyShownStore
             throw;
         }
 
+        // It's more efficient to reuse the upload time from the listing itself than to query another store for that information
         var guess = listings.FirstOrDefault();
         var guessUploadTime = guess == null ? 0 : new DateTimeOffset(guess.UpdatedAt).ToUnixTimeMilliseconds();
         return new CurrentlyShown
         {
             WorldId = query.WorldId,
             ItemId = query.ItemId,
-            LastUploadTimeUnixMilliseconds = Math.Max(guessUploadTime, lastUpdated),
+            LastUploadTimeUnixMilliseconds = guessUploadTime,
             UploadSource = guess?.Source ?? "",
             Listings = listings,
         };
@@ -105,16 +100,6 @@ public class CurrentlyShownStore : ICurrentlyShownStore
         var worldItemPairs = worldIds.SelectMany(worldId =>
                 itemIds.Select(itemId => new WorldItemPair(worldId, itemId)))
             .ToList();
-
-        // Get all update times from Redis
-        activity?.AddEvent(new ActivityEvent("GetLastUpdated"));
-        var lastUpdatedByItem = new Dictionary<WorldItemPair, long>();
-        var lastUpdatedTasks =
-            await Task.WhenAll(worldItemPairs.Select(t => GetLastUpdated(t.WorldId, t.ItemId)));
-        foreach (var (key, lastUpdated) in worldItemPairs.Zip(lastUpdatedTasks))
-        {
-            lastUpdatedByItem[key] = lastUpdated;
-        }
 
         // Attempt to retrieve listings from Postgres
         activity?.AddEvent(new ActivityEvent("GetListings"));
@@ -135,34 +120,22 @@ public class CurrentlyShownStore : ICurrentlyShownStore
         return worldItemPairs
             .Select(key =>
             {
-                var lastUpdated = lastUpdatedByItem[key];
-                if (lastUpdated == 0)
-                {
-                    return null;
-                }
-
                 var listings = listingsByItem[key];
 
+                // It's more efficient to reuse the upload time from the listing itself than to query another store for that information
                 var guess = listings.FirstOrDefault();
                 var guessUploadTime = guess == null ? 0 : new DateTimeOffset(guess.UpdatedAt).ToUnixTimeMilliseconds();
                 return new CurrentlyShown
                 {
                     WorldId = key.WorldId,
                     ItemId = key.ItemId,
-                    LastUploadTimeUnixMilliseconds = Math.Max(guessUploadTime, lastUpdated),
+                    LastUploadTimeUnixMilliseconds = guessUploadTime,
                     UploadSource = guess?.Source ?? "",
-                    // I don't remember why/if this needs to be a concrete type but I
-                    // think this has a fast path internally anyways.
+                    // I don't remember why/if this needs to be a concrete type, but I
+                    // think this has a fast path internally anyway.
                     Listings = listings.ToList(),
                 };
-            })
-            .Where(cs => cs is not null);
-    }
-
-    private async Task<long> GetLastUpdated(int worldId, int itemId)
-    {
-        var timestamp = await _worldItemUploadStore.GetUploadTime(worldId, itemId);
-        return !timestamp.HasValue ? 0 : Convert.ToInt64(timestamp.Value);
+            });
     }
 
     private Task SetLastUpdated(int worldId, int itemId, long timestamp)
