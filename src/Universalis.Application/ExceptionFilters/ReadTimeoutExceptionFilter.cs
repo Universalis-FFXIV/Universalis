ac5d3b36b71bf24b1d65d58f897f0567cf3b4a49
karashiiro <49822414+karashiiro@users.noreply.github.com>
2024-07-28 09:16:10 -0700

    Add exception filters for timeouts

---
+++ src/Universalis.Application/ExceptionFilters/ReadTimeoutExceptionFilter.cs
@@ -0,0 +1,26 @@
+﻿using Cassandra;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+using Microsoft.Extensions.Logging;
+
+namespace Universalis.Application.ExceptionFilters;
+
+public class ReadTimeoutExceptionFilter : IExceptionFilter
+{
+    private readonly ILogger _logger;
+
+    public ReadTimeoutExceptionFilter(ILoggerFactory loggerFactory)
+    {
+        _logger = loggerFactory.CreateLogger<ReadTimeoutExceptionFilter>();
+    }
+
+    public void OnException(ExceptionContext context)
+    {
+        if (context.Exception is not ReadTimeoutException) return;
+        _logger.LogWarning(context.Exception, "Cassandra read request was cancelled");
+        context.Result = new StatusCodeResult(504);
+        context.ExceptionHandled = true;
+        context.HttpContext.Response.StatusCode = StatusCodes.Status504GatewayTimeout;
+    }
+}
---
+++ src/Universalis.Application/ExceptionFilters/TimeoutExceptionFilter.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+using Microsoft.Extensions.Logging;
+using System;
+
+namespace Universalis.Application.ExceptionFilters;
+
+public class TimeoutExceptionFilter : IExceptionFilter
+{
+    private readonly ILogger _logger;
+
+    public TimeoutExceptionFilter(ILoggerFactory loggerFactory)
+    {
+        _logger = loggerFactory.CreateLogger<TimeoutExceptionFilter>();
+    }
+
+    public void OnException(ExceptionContext context)
+    {
+        if (context.Exception is not TimeoutException) return;
+        _logger.LogWarning("Request timed out");
+        context.Result = new StatusCodeResult(504);
+        context.ExceptionHandled = true;
+        context.HttpContext.Response.StatusCode = StatusCodes.Status504GatewayTimeout;
+    }
+}
